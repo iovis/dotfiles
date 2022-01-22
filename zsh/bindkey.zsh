ee786fd178d78553d07ccc0b935df5f5314b47bd
David Marchante <davidmarchan@gmail.com>
2022-01-23 00:22:42 +0100

    [zsh] Fix history bindings in linux

--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -4,10 +4,12 @@ bindkey -e
 autoload -U up-line-or-beginning-search
 zle -N up-line-or-beginning-search
 bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "$key[Up]" up-line-or-beginning-search
 
 autoload -U down-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey "^[[B" down-line-or-beginning-search # Down
+bindkey "$key[Down]" down-line-or-beginning-search
 
 autoload -U edit-command-line
 zle -N edit-command-line
