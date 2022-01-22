adf506400efd6688360788a63e5ff991c192e71d
David Marchante <davidmarchan@gmail.com>
2022-01-22 15:19:51 -0500

    [vim] Clean nvim-cmp bindings

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -41,14 +41,12 @@ cmp.setup({
     end,
   },
   mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    -- ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ["<C-b>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.config.disable,
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-y>"] = cmp.mapping.confirm(),
+    ["<CR>"]  = cmp.config.disable,
     ["<Tab>"] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
