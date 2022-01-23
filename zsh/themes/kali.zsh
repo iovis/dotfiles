cc47a5ca10ecea3034ed2fc469e4ca75d5dd98fd
David Marchante <davidmarchan@gmail.com>
2022-01-23 13:55:30 -0500

    [zsh] Add kali theme
    https://gist.github.com/Anon-Exploiter/4e12193df0099183d18720c6471d571a

---
+++ zsh/themes/kali.zsh
@@ -0,0 +1,5 @@
+prompt_symbol=㉿
+[ "$EUID" -eq 0 ] && prompt_symbol=💀
+
+PROMPT=$'%F{%(#.blue.green)}┌──${debian_chroot:+($debian_chroot)─}${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV))─}(%B%F{%(#.red.blue)}%n$prompt_symbol%m%b%F{%(#.blue.green)})-[%B%F{reset}%(6~.%-1~/…/%4~.%5~)%b%F{%(#.blue.green)}]\n└─%B%(#.%F{red}#.%F{blue}$)%b%F{reset} '
+RPROMPT=$'%(?.. %? %F{red}%B⨯%b%F{reset})%(1j. %j %F{yellow}%B⚙%b%F{reset}.)'
