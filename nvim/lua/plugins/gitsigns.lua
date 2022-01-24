cc1d313ba734d0ca707595c8e1623f1107c43d9d
David Marchante <davidmarchan@gmail.com>
2022-01-23 19:09:36 -0500

    [vim] Clean gitsigns keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,13 +1,11 @@
+local u = require("utils")
+
 require("gitsigns").setup({
-  keymaps = {
-    noremap = true,
-
-    ["n ]c"] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'" },
-    ["n [c"] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'" },
-
-    ["n +q"] = '<cmd>lua require("gitsigns").toggle_current_line_blame()<CR>',
-  },
   current_line_blame_opts = {
     delay = 100,
   },
 })
+
+u.nmap("]c", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<cr>'", { expr = true })
+u.nmap("[c", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<cr>'", { expr = true })
+u.nmap("+q", "<cmd>Gitsigns toggle_current_line_blame<cr>")
