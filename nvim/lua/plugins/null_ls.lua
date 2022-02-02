229eeafbb554be7911b1c477b44b358b232f4a0a
David Marchante <davidmarchan@gmail.com>
2022-02-02 16:53:14 -0500

    [vim] Add note about formatting conflicts

--- nvim/lua/plugins/null_ls.lua
+++ nvim/lua/plugins/null_ls.lua
@@ -3,6 +3,14 @@ if not status_ok then
   return
 end
 
+-- NOTE: Formatting conflicts
+-----------------------------
+-- If when formatting there are multiple possible candidates for
+-- formatting, Neovim will ask you which one to use. If you want to use
+-- null-ls formatting you can disable the conflicting language's
+-- formatting capabilities.
+-- See: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts
+
 -- local completion = null_ls.builtins.completion
 local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
