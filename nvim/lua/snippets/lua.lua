4534de11925d232108f0ea092f2ad8a01bb6813e
David Marchante <davidmarchan@gmail.com>
2022-04-29 00:35:28 -0400

    [vim] Add lua snippets for lua snippets

--- nvim/lua/snippets/lua.lua
+++ nvim/lua/snippets/lua.lua
@@ -1,36 +1,37 @@
-local ls = require("luasnip")
-local snippet = ls.snippet
-local sn = ls.snippet_node
-local isn = ls.indent_snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
-local r = ls.restore_node
-local events = require("luasnip.util.events")
-local ai = require("luasnip.nodes.absolute_indexer")
-
 return {
-  snippet("snipinit", {
-    t({
-      'local ls = require("luasnip")',
-      "local snippet = ls.snippet",
-      "local sn = ls.snippet_node",
-      "local isn = ls.indent_snippet_node",
-      "local t = ls.text_node",
-      "local i = ls.insert_node",
-      "local f = ls.function_node",
-      "local c = ls.choice_node",
-      "local d = ls.dynamic_node",
-      "local r = ls.restore_node",
-      'local events = require("luasnip.util.events")',
-      'local ai = require("luasnip.nodes.absolute_indexer")',
-      "",
-      "return {",
-      "  ",
-    }),
-    i(1, "-- Your snippets here"),
-    t({ "", "}" }),
-  }),
+  s(
+    "snipinit",
+    fmta(
+      [[
+        return {
+          s("<>", <>),
+        }
+      ]],
+      {
+        i(1),
+        i(0),
+      }
+    )
+  ),
+  s("s", fmt([[s("{}", {}),]], { i(1), i(0) })),
+  s(
+    "fmt",
+    fmta("fmt(<>, {<>})", {
+      c(1, {
+        sn(nil, fmt([["{}"]], i(1))),
+        sn(
+          nil,
+          fmt(
+            [=[
+              [[
+                {}
+              ]]
+            ]=],
+            i(1)
+          )
+        ),
+      }),
+      i(0),
+    })
+  ),
 }
