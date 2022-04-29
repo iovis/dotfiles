ca0bdf7598eaa8aca6004a060da8e94cf1fbb03f
David Marchante <davidmarchan@gmail.com>
2022-04-29 01:40:30 -0400

    [vim] Migrate JSON snippets

---
+++ nvim/lua/snippets/json.lua
@@ -0,0 +1,17 @@
+return {
+  s(
+    "projectionist",
+    fmta(
+      [[
+      {
+        "*": {
+          "start": "<>",
+          "console": "<>",
+          "dispatch": "<>"
+        }
+      }
+      ]],
+      { i(1, "npm start"), i(2, "node"), i(3, "npm test") }
+    )
+  ),
+}
