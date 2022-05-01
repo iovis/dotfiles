6887de8064f1aec2887e8c9e763efa7e1e711008
David Marchante <davidmarchan@gmail.com>
2022-05-01 12:30:20 -0400

    [vim] Add Rust snippets for functions

--- nvim/lua/snippets/rust.lua
+++ nvim/lua/snippets/rust.lua
@@ -1,4 +1,38 @@
+local rust_fn = function()
+  return sn(
+    nil,
+    fmta(
+      [[
+        fn <fname>(<args>)<ret_type> {
+            <body>
+        }
+      ]],
+      {
+        fname = i(1, "fname"),
+        args = i(2),
+        ret_type = c(3, {
+          t(""),
+          {
+            t(" -> "),
+            i(1),
+          },
+        }),
+        body = i(4, "unimplemented!()"),
+      }
+    )
+  )
+end
+
 return {
+  s({ trig = "fn", dscr = "function" }, d(1, rust_fn), { condition = conds.line_begin }),
+  s({ trig = "pfn", dscr = "pub function" }, fmt("pub {}", { d(1, rust_fn) }), { condition = conds.line_begin }),
+  s({ trig = "afn", dscr = "async function" }, fmt("async {}", { d(1, rust_fn) }), { condition = conds.line_begin }),
+  s(
+    { trig = "pafn", dscr = "pub async function" },
+    fmt("pub async {}", { d(1, rust_fn) }),
+    { condition = conds.line_begin }
+  ),
+  s("fd", fmt("{field}: {value},", { field = i(1, "field"), value = i(2, "value") }), { condition = conds.line_begin }),
   s(
     { trig = "tt", dscr = "Tokio test" },
     fmta(
