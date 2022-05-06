51e4bf06664e45a0488f4f36176690a98ad87f7d
David Marchante <davidmarchan@gmail.com>
2022-05-06 01:09:38 -0400

    [vim] Tweak lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,6 +1,22 @@
 require("lualine").setup({
   options = {
-    theme = "jellybeans",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    theme = "base16",
+  },
+  sections = {
+    lualine_a = {},
+    lualine_b = {
+      {
+        "filetype",
+        icon_only = true,
+      },
+      "filename",
+    },
+    lualine_c = { "branch" },
+    lualine_x = { "diff", "diagnostics", "fileformat" },
+    lualine_y = { "progress", "location" },
+    lualine_z = {},
   },
   extensions = { "fugitive", "nvim-tree", "quickfix", "fzf" },
 })
