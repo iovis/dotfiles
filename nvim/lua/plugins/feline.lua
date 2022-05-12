30c67fbb7bb76476b6faa52035fadf4d4b615192
David Marchante <davidmarchan@gmail.com>
2022-05-11 23:40:02 -0400

    [vim] Generate unique names for buffers in Feline

--- nvim/lua/plugins/feline.lua
+++ nvim/lua/plugins/feline.lua
@@ -110,8 +110,67 @@ local main_icon = {
 }
 
 -- File name
+-- Get the names of all current listed buffers
+local function get_current_filenames()
+  local listed_buffers = vim.tbl_filter(function(bufnr)
+    return vim.bo[bufnr].buflisted and vim.api.nvim_buf_is_loaded(bufnr)
+  end, vim.api.nvim_list_bufs())
+
+  return vim.tbl_map(vim.api.nvim_buf_get_name, listed_buffers)
+end
+
+-- Get unique name for the current buffer
+local function get_unique_filename(filename, shorten)
+  local filenames = vim.tbl_filter(function(filename_other)
+    return filename_other ~= filename
+  end, get_current_filenames())
+
+  if shorten then
+    filename = vim.fn.pathshorten(filename)
+    filenames = vim.tbl_map(vim.fn.pathshorten, filenames)
+  end
+
+  -- Reverse filenames in order to compare their names
+  filename = string.reverse(filename)
+  filenames = vim.tbl_map(string.reverse, filenames)
+
+  local index
+
+  -- For every other filename, compare it with the name of the current file char-by-char to
+  -- find the minimum index `i` where the i-th character is different for the two filenames
+  -- After doing it for every filename, get the maximum value of `i`
+  if next(filenames) then
+    index = math.max(unpack(vim.tbl_map(function(filename_other)
+      for i = 1, #filename do
+        -- Compare i-th character of both names until they aren't equal
+        if filename:sub(i, i) ~= filename_other:sub(i, i) then
+          return i
+        end
+      end
+      return 1
+    end, filenames)))
+  else
+    index = 1
+  end
+
+  -- Iterate backwards (since filename is reversed) until a "/" is found
+  -- in order to show a valid file path
+  while index <= #filename do
+    if filename:sub(index, index) == "/" then
+      index = index - 1
+      break
+    end
+
+    index = index + 1
+  end
+
+  return string.reverse(string.sub(filename, 1, index))
+end
+
 local file_name_provider = function()
-  local filename = vim.fn.expand("%:t")
+  local filename = vim.api.nvim_buf_get_name(0)
+  filename = get_unique_filename(filename, true)
+
   local extension = vim.fn.expand("%:e")
   local icon = require("nvim-web-devicons").get_icon(filename, extension)
 
