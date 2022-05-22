5628653df543881337dc88c348359c9fad1444a2
David Marchante <davidmarchan@gmail.com>
2022-05-22 18:27:23 -0400

    [vim] Add binding to toggle diagnostics

--- nvim/lua/diagnostics.lua
+++ nvim/lua/diagnostics.lua
@@ -1,24 +1,15 @@
 local u = require("utils")
 
-local augroup = vim.api.nvim_create_augroup("diagnostics", { clear = true })
-vim.api.nvim_create_autocmd("DiagnosticChanged", {
-  pattern = "*",
-  callback = function()
-    vim.diagnostic.setloclist({ open = false })
-  end,
-  group = augroup,
-  desc = "Set diagnostics on location list",
-})
-
+---- UI
 u.highlight("DiagnosticSignError", { fg = "#ab4642" })
-u.highlight("DiagnosticSignWarn",  { fg = "#f7ca88" })
-u.highlight("DiagnosticSignInfo",  { fg = "#7cafc2" })
-u.highlight("DiagnosticSignHint",  { fg = "#7cafc2" })
+u.highlight("DiagnosticSignWarn", { fg = "#f7ca88" })
+u.highlight("DiagnosticSignInfo", { fg = "#7cafc2" })
+u.highlight("DiagnosticSignHint", { fg = "#7cafc2" })
 
 u.highlight("DiagnosticVirtualTextError", { fg = "#ab4642" })
-u.highlight("DiagnosticVirtualTextWarn",  { fg = "#f7ca88" })
-u.highlight("DiagnosticVirtualTextInfo",  { fg = "#7cafc2" })
-u.highlight("DiagnosticVirtualTextHint",  { fg = "#7cafc2" })
+u.highlight("DiagnosticVirtualTextWarn", { fg = "#f7ca88" })
+u.highlight("DiagnosticVirtualTextInfo", { fg = "#7cafc2" })
+u.highlight("DiagnosticVirtualTextHint", { fg = "#7cafc2" })
 
 local signs = {
   { name = "DiagnosticSignError", text = "" },
@@ -35,6 +26,7 @@ for _, sign in ipairs(signs) do
   })
 end
 
+---- Configuration
 local config = {
   float = {
     focusable = false,
@@ -53,3 +45,29 @@ local config = {
 }
 
 vim.diagnostic.config(config)
+
+---- Set Diagnostics on location list
+local augroup = vim.api.nvim_create_augroup("diagnostics", { clear = true })
+vim.api.nvim_create_autocmd("DiagnosticChanged", {
+  pattern = "*",
+  callback = function()
+    vim.diagnostic.setloclist({ open = false })
+  end,
+  group = augroup,
+  desc = "Set diagnostics on location list",
+})
+
+---- Toggle diagnostics
+vim.g.diagnostics_visible = true
+
+local toggle_diagnostics = function()
+  if vim.g.diagnostics_visible then
+    vim.g.diagnostics_visible = false
+    vim.diagnostic.disable()
+  else
+    vim.g.diagnostics_visible = true
+    vim.diagnostic.enable()
+  end
+end
+
+vim.keymap.set("n", "+d", toggle_diagnostics, { desc = "toggle diagnostics" })
