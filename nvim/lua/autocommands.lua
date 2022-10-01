72b5430bd930b9ab914010051edd5662c825d355
David Marchante <davidmarchan@gmail.com>
2022-10-01 18:00:17 -0400

    [vim] Generate augroups within autocmds

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -3,20 +3,18 @@ local u = require("utils")
 ---- Highlight on yank
 u.highlight("HighlightedYankRegion", { fg = "#262626", bg = "#8fafd7" })
 
-vim.api.nvim_create_augroup("highlighted_yank", { clear = true })
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   pattern = "*",
-  group = "highlighted_yank",
+  group = vim.api.nvim_create_augroup("highlighted_yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank({ higroup = "HighlightedYankRegion", timeout = 300 })
   end,
 })
 
 ---- Auto create intermediate directories if they don't exist
-vim.api.nvim_create_augroup("autocreate_dir", { clear = true })
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = "*",
-  group = "autocreate_dir",
+  group = vim.api.nvim_create_augroup("autocreate_dir", { clear = true }),
   callback = function(ctx)
     local dir = vim.fn.fnamemodify(ctx.file, ":p:h")
     vim.fn.mkdir(dir, "p")
