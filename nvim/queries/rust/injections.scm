2095469681cfdd95b003277a4d4b7c98216575ab
David Marchante <davidmarchan@gmail.com>
2022-10-02 14:49:26 -0400

    [vim] Add sql injections to rust

---
+++ nvim/queries/rust/injections.scm
@@ -0,0 +1,9 @@
+; Inject into sqlx::query!(r#"..."#, ...) as sql
+(macro_invocation
+  (scoped_identifier
+    path: (identifier) @_path (#eq? @_path "sqlx")
+    name: (identifier) @_name (#eq? @_name "query"))
+
+  (token_tree
+    (raw_string_literal) @sql)
+  (#offset! @sql 1 0 0 0))
