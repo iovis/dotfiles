2f2eee3ecf397370af351ca647bb83f1f89b0584
David Marchante <davidmarchan@gmail.com>
2022-10-03 12:27:04 -0400

    [vim] Allow for regular strings for sql injections in rust

--- nvim/queries/rust/injections.scm
+++ nvim/queries/rust/injections.scm
@@ -1,9 +1,12 @@
-; Inject into sqlx::query!(r#"..."#, ...) as sql
+; Inject into sqlx::query!("{}", ...) as sql
 (macro_invocation
   (scoped_identifier
     path: (identifier) @_path (#eq? @_path "sqlx")
     name: (identifier) @_name (#eq? @_name "query"))
 
   (token_tree
-    (raw_string_literal) @sql)
+    [
+     (raw_string_literal)
+     (string_literal)
+    ] @sql)
   (#offset! @sql 1 0 0 0))
