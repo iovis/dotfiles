8f4ced438a6152e220bc542d12d1d68312141291
David Marchante <davidmarchan@gmail.com>
2022-12-12 00:16:06 -0500

    [vim] Add rust test nocapture strategy

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -5,8 +5,9 @@ u.lsp_autoformat()
 vim.keymap.set("n", "m<cr>", "<cmd>Tux cargo check --all-targets<cr>", { buffer = true })
 vim.keymap.set("n", "s<cr>", "<cmd>Tux cargo run<cr>", { buffer = true })
 
-vim.keymap.set("n", "<leader>sr", "<cmd>RustReloadWorkspace<cr>", { buffer = true })
+vim.keymap.set("n", "<leader>sn", "<cmd>TestNearest -strategy=rust_print<cr>", { buffer = true })
 vim.keymap.set("n", "<leader>sp", "<cmd>TestNearest -strategy=rust_log<cr>", { buffer = true })
+vim.keymap.set("n", "<leader>sr", "<cmd>RustReloadWorkspace<cr>", { buffer = true })
 vim.keymap.set("n", "<leader>sw", "<cmd>TuxBg! cw<cr>", { buffer = true })
 
 vim.keymap.set("n", "+R", ":RustDocs<space>", { buffer = true })
--- nvim/.config/nvim/after/plugin/vim-test.vim
+++ nvim/.config/nvim/after/plugin/vim-test.vim
@@ -12,6 +12,10 @@ function! RustLogStrategy(cmd)
   execute 'Tux TEST_LOG=enabled ' . a:cmd . ' | bunyan'
 endfunction
 
+function! RustPrintStrategy(cmd)
+  execute 'Tux ' . a:cmd . ' --nocapture'
+endfunction
+
 function! TestProfStrategy(cmd)
   execute 'Tux FPROF=1 FDOC=1 ' . a:cmd
 endfunction
@@ -19,6 +23,7 @@ endfunction
 let g:test#custom_strategies = {
       \ 'tux': function('TuxStrategy'),
       \ 'rust_log': function('RustLogStrategy'),
+      \ 'rust_print': function('RustPrintStrategy'),
       \ 'test_prof': function('TestProfStrategy'),
       \}
 
