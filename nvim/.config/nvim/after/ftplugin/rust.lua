1cbe8a9ce809d9360633c8a038c483faf5e47ec9
David Marchante <davidmarchan@gmail.com>
2022-12-29 18:18:49 -0500

    [vim] Add some more Rust bindings

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -2,12 +2,13 @@ local u = require("user.utils")
 
 u.lsp_autoformat()
 
+vim.keymap.set("n", "c<cr>", "<cmd>Tux irust<cr>", { buffer = true })
 vim.keymap.set("n", "m<cr>", "<cmd>Tux cargo check --all-targets<cr>", { buffer = true })
 vim.keymap.set("n", "s<cr>", "<cmd>Tux cargo run<cr>", { buffer = true })
 
 vim.keymap.set("n", "<leader>sn", "<cmd>TestNearest -strategy=rust_print<cr>", { buffer = true })
 vim.keymap.set("n", "<leader>sp", "<cmd>TestNearest -strategy=rust_log<cr>", { buffer = true })
 vim.keymap.set("n", "<leader>sr", "<cmd>RustReloadWorkspace<cr>", { buffer = true })
-vim.keymap.set("n", "<leader>sw", "<cmd>TuxBg! cw<cr>", { buffer = true })
+vim.keymap.set("n", "<leader>sw", "<cmd>Tux cargo watch --clear -x check -x 'nextest run'<cr>", { buffer = true })
 
 vim.keymap.set("n", "+R", ":RustDocs<space>", { buffer = true })
