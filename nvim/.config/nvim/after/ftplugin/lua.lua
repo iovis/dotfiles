6f9950168a4c07a1abbd2d3d599f060250e1fdc8
David Marchante <davidmarchan@gmail.com>
2022-12-30 12:39:19 -0500

    [vim] Add binding to re-run M.config() for current plugin

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -4,3 +4,13 @@ u.lsp_autoformat()
 
 vim.keymap.set("n", "<leader>so", "<cmd>source %<cr>", { buffer = true })
 vim.keymap.set("n", "<leader>P", ":lua =", { buffer = true })
+
+-- Re-source `config()` for the current plugin
+vim.keymap.set("n", "<leader>sr", function()
+  -- Get the name of the current buffer's file
+  local filename = vim.api.nvim_buf_get_name(0)
+  local base_name = string.match(filename, "([^/\\]+)%.[^.]+$")
+
+  -- Require the plugin and run its config function
+  require("plugins." .. base_name).config()
+end, { buffer = true })
