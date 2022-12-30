691129bde0f315b64e5fc0bea12310803c1abf70
David Marchante <davidmarchan@gmail.com>
2022-12-30 18:00:40 -0500

    [vim] Update rust snippets for AoC

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -14,7 +14,7 @@ vim.keymap.set("n", "+R", ":RustDocs<space>", { buffer = true })
 
 if string.match(vim.fn.expand("%"), "examples/") then
   -- if inside example, run it
-  vim.keymap.set("n", "s<cr>", "<cmd>Tux cargo run --example %:t:r<cr>", { buffer = true })
+  vim.keymap.set("n", "s<cr>", "<cmd>Tux cargo run -q --example %:t:r<cr>", { buffer = true })
 else
   vim.keymap.set("n", "s<cr>", "<cmd>Tux cargo run<cr>", { buffer = true })
 end
