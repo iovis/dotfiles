e9b5a13f17972945305855953af6da9c817c821c
David Marchante <davidmarchan@gmail.com>
2022-12-30 16:18:16 -0500

    [vim] If inside rust example, run that instead

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -4,7 +4,6 @@ u.lsp_autoformat()
 
 vim.keymap.set("n", "c<cr>", "<cmd>Tux irust<cr>", { buffer = true })
 vim.keymap.set("n", "m<cr>", "<cmd>Tux cargo check --all-targets<cr>", { buffer = true })
-vim.keymap.set("n", "s<cr>", "<cmd>Tux cargo run<cr>", { buffer = true })
 
 vim.keymap.set("n", "<leader>sn", "<cmd>TestNearest -strategy=rust_print<cr>", { buffer = true })
 vim.keymap.set("n", "<leader>sp", "<cmd>TestNearest -strategy=rust_log<cr>", { buffer = true })
@@ -12,3 +11,10 @@ vim.keymap.set("n", "<leader>sr", "<cmd>RustReloadWorkspace<cr>", { buffer = tru
 vim.keymap.set("n", "<leader>sw", "<cmd>Tux cargo watch --clear -x check -x 'nextest run'<cr>", { buffer = true })
 
 vim.keymap.set("n", "+R", ":RustDocs<space>", { buffer = true })
+
+if string.match(vim.fn.expand("%"), "examples/") then
+  -- if inside example, run it
+  vim.keymap.set("n", "s<cr>", "<cmd>Tux cargo run --example %:t:r<cr>", { buffer = true })
+else
+  vim.keymap.set("n", "s<cr>", "<cmd>Tux cargo run<cr>", { buffer = true })
+end
