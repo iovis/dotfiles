39c9d6fc37c3f371292779823a6d49a440e8e041
David Marchante <davidmarchan@gmail.com>
2023-01-04 14:38:01 -0500

    [vim] Don't lazy load plugins that define filetypes

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,7 +1,11 @@
 return {
-  "NoahTheDuke/vim-just",
+  "NoahTheDuke/vim-just", -- Not lazy because it calculates filetypes
+  "tpope/vim-git", -- Not lazy because it calculates filetypes
+  "tpope/vim-rhubarb", -- Not lazy because it calculates filetypes
+
   { "andrewradev/splitjoin.vim", event = "VeryLazy" },
   { "christoomey/vim-sort-motion", event = "VeryLazy" },
+  { "dstein64/vim-startuptime", cmd = "StartupTime" },
   { "iovis/browsers_castle", event = "VeryLazy" },
   { "iovis/substitute.vim", event = "VeryLazy" },
   { "jparise/vim-graphql", event = "VeryLazy" },
@@ -13,19 +17,12 @@ return {
   { "tpope/vim-dotenv", event = "VeryLazy" },
   { "tpope/vim-eunuch", event = "VeryLazy" },
   { "tpope/vim-fugitive", event = "VeryLazy" },
-  { "tpope/vim-git", event = "VeryLazy" },
   { "tpope/vim-ragtag", event = "VeryLazy" },
   { "tpope/vim-rails", event = "VeryLazy" },
   { "tpope/vim-rake", event = "VeryLazy" },
   { "tpope/vim-rbenv", event = "VeryLazy" },
   { "tpope/vim-repeat", event = "VeryLazy" },
-  { "tpope/vim-rhubarb", event = "VeryLazy" },
   { "tpope/vim-scriptease", event = "VeryLazy" },
   { "tpope/vim-speeddating", event = "VeryLazy" },
   { "vim-test/vim-test", event = "VeryLazy" },
-
-  {
-    "dstein64/vim-startuptime",
-    cmd = "StartupTime",
-  },
 }
