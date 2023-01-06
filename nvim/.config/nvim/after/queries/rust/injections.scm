307878783bdf12967cf7374f1995219488c09d25
David Marchante <davidmarchan@gmail.com>
2023-01-06 13:44:17 -0500

    [vim] Move TS queries to after/queries

--- nvim/.config/nvim/after/queries/rust/injections.scm
+++ nvim/.config/nvim/after/queries/rust/injections.scm
@@ -1,12 +1,13 @@
+; extends
+
 ; Inject into sqlx::query!("{}", ...) as sql
 (macro_invocation
   (scoped_identifier
     path: (identifier) @_path (#eq? @_path "sqlx")
     name: (identifier) @_name (#eq? @_name "query"))
 
-  (token_tree
-    [
-     (raw_string_literal)
-     (string_literal)
-    ] @sql)
+  (token_tree [
+    (raw_string_literal)
+    (string_literal)
+  ] @sql)
   (#offset! @sql 1 0 0 0))
--- nvim/.config/nvim/queries/ecma/injections.scm
+++
@@ -1,6 +0,0 @@
-; (inherited by typescript)
-; Inject into /* GraphQL */`<gql>` as graphql
-((comment) @_gql_comment
-   (#eq? @_gql_comment "/* GraphQL */")
-   (expression_statement (template_string) @graphql
-   (#offset! @graphql 0 1 0 -1)))
