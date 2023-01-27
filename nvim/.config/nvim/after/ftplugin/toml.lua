fd7be6d5a2e8543a762a961e548d663ad751e39b
David Marchante <davidmarchan@gmail.com>
2023-01-27 13:40:56 -0500

    [muxi] Open editor with +ZenMode

--- nvim/.config/nvim/after/ftplugin/toml.lua
+++ nvim/.config/nvim/after/ftplugin/toml.lua
@@ -1,13 +1,4 @@
-local u = require("config.utils")
-
 ---- muxi
 if string.match(vim.fn.expand("%"), "muxi/") then
   vim.keymap.set("n", "m<cr>", ":silent !muxi init<cr>", { buffer = true })
-
-  -- If only buffer, enable zen-mode
-  if u.is_only_buffer() then
-    vim.schedule(function()
-      require("zen-mode").toggle({})
-    end)
-  end
 end
