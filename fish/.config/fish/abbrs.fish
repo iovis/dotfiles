bc3ad66b6e793d81c3e67f36bf3c0102809fa676
David Marchante <davidmarchan@gmail.com>
2023-02-07 21:25:34 -0500

    [fish] Clean up abbr

--- fish/.config/fish/abbrs.fish
+++ fish/.config/fish/abbrs.fish
@@ -1,12 +1,13 @@
 ## directories
-abbr -a -- - 'cd -'
-abbr ...... 'cd ../../../../..'
-abbr ..... 'cd ../../../..'
-abbr .... 'cd ../../..'
-abbr ... 'cd ../..'
+abbr -- -   'cd -'
+abbr ...    'cd ../..'
+abbr ....   'cd ../../..'
 
 ## ruby
-abbr RED "RAILS_ENV=development"
-abbr REP "RAILS_ENV=production"
-abbr REQ "RAILS_ENV=qa"
-abbr RET "RAILS_ENV=test"
+abbr RED RAILS_ENV=development
+abbr REP RAILS_ENV=production
+abbr REQ RAILS_ENV=qa
+abbr RET RAILS_ENV=test
+
+## rust
+abbr RT RUST_BACKTRACE=1
