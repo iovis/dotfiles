653a896d776c225e3d3ce6fcb74368f7fd3f9ad2
David Marchante <davidmarchan@gmail.com>
2023-02-11 16:15:37 -0500

    [vim] bufferline catppuccin

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,66 +1,60 @@
-local M = {
+return {
   "akinsho/nvim-bufferline.lua",
   -- enabled = false,
   event = "BufAdd",
+  config = function()
+    -- local c = require("config.highlights").colors
+
+    require("bufferline").setup({
+      highlights = require("catppuccin.groups.integrations.bufferline").get(),
+      -- highlights = { indicator_selected = { fg = c.blue } },
+      options = {
+        mode = "tabs",
+        numbers = "none",
+        -- indicator = {
+        --   style = "underline", -- 'icon' | 'underline' | 'none'
+        -- },
+        -- custom_filter = function(buf_number, _)
+        --   -- filter out filetypes you don't want to see
+        --   return vim.bo[buf_number].filetype ~= "qf"
+        -- end,
+        -- numbers = function(opts)
+        --   return string.format("%s.", opts.ordinal)
+        -- end,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        separator_style = { "", "" },
+        always_show_bufferline = false,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "",
+            highlight = "Directory",
+            text_align = "center",
+          },
+          {
+            filetype = "NvimTree",
+            text = "",
+            highlight = "Directory",
+            text_align = "center",
+          },
+          {
+            filetype = "undotree",
+            text = "Undo Tree",
+            highlight = "Directory",
+            text_align = "center",
+          },
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<tab>", "<cmd>BufferLineCycleNext<cr>")
+    vim.keymap.set("n", "<s-tab>", "<cmd>BufferLineCyclePrev<cr>")
+
+    for i = 1, 9 do
+      vim.keymap.set("n", "<leader>" .. i, function()
+        require("bufferline").go_to_buffer(i, true)
+      end, { desc = "Go to buffer " .. i })
+    end
+  end,
 }
-
-function M.config()
-  local c = require("config.highlights").colors
-
-  require("bufferline").setup({
-    highlights = {
-      indicator_selected = {
-        fg = c.blue,
-      },
-    },
-    options = {
-      mode = "tabs",
-      numbers = "none",
-      -- indicator = {
-      --   style = "underline", -- 'icon' | 'underline' | 'none'
-      -- },
-      -- custom_filter = function(buf_number, _)
-      --   -- filter out filetypes you don't want to see
-      --   return vim.bo[buf_number].filetype ~= "qf"
-      -- end,
-      -- numbers = function(opts)
-      --   return string.format("%s.", opts.ordinal)
-      -- end,
-      show_buffer_close_icons = false,
-      show_close_icon = false,
-      separator_style = { "", "" },
-      always_show_bufferline = false,
-      offsets = {
-        {
-          filetype = "neo-tree",
-          text = "",
-          highlight = "Directory",
-          text_align = "center",
-        },
-        {
-          filetype = "NvimTree",
-          text = "",
-          highlight = "Directory",
-          text_align = "center",
-        },
-        {
-          filetype = "undotree",
-          text = "Undo Tree",
-          highlight = "Directory",
-          text_align = "center",
-        },
-      },
-    },
-  })
-
-  vim.keymap.set("n", "<tab>", "<cmd>BufferLineCycleNext<cr>")
-  vim.keymap.set("n", "<s-tab>", "<cmd>BufferLineCyclePrev<cr>")
-
-  for i = 1, 9 do
-    vim.keymap.set("n", "<leader>" .. i, function()
-      require("bufferline").go_to_buffer(i, true)
-    end, { desc = "Go to buffer " .. i })
-  end
-end
-
-return M
