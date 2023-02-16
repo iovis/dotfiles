0997d49b101e791fad26fc12a2428cf893ca66e6
David Marchante <davidmarchan@gmail.com>
2023-02-16 18:44:28 -0500

    [vim] Tweak colorizer a bit

--- nvim/.config/nvim/lua/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,6 +1,19 @@
 return {
   "NvChad/nvim-colorizer.lua",
+  -- enabled = false,
   config = function()
-    require("colorizer").setup()
+    require("colorizer").setup({
+      filetypes = {
+        "css",
+        "html",
+        "scss",
+        "svelte",
+      },
+      user_default_options = {
+        names = false,
+        RRGGBBAA = true,
+        mode = "virtualtext",
+      },
+    })
   end,
 }
