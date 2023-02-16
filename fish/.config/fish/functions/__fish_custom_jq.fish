c87846e3837ed0b8fc4bacc0a7a7b0703c6a6d0e
David Marchante <davidmarchan@gmail.com>
2023-02-16 09:46:12 -0500

    [fish] Add binding for 'jq' filter

---
+++ fish/.config/fish/functions/__fish_custom_jq.fish
@@ -0,0 +1,3 @@
+function __fish_custom_jq
+    fish_commandline_append "| jq "
+end
--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -6,4 +6,5 @@ function fish_user_key_bindings
 
     # grep command
     bind \eg '__fish_custom_grep; commandline -f end-of-line'
+    bind \ej '__fish_custom_jq; commandline -f end-of-line'
 end
