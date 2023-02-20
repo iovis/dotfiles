ed5017c053509cb661517e3729fa27b09c8600b3
David Marchante <davidmarchan@gmail.com>
2023-02-20 02:38:29 -0500

    [vim] Update lspsaga

--- nvim/.config/nvim/lua/plugins/lspsaga.lua
+++ nvim/.config/nvim/lua/plugins/lspsaga.lua
@@ -5,12 +5,11 @@ return {
       beacon = {
         enable = false,
       },
-      code_action = {
-        extend_gitsigns = false,
-      },
+      -- code_action = {
+      --   extend_gitsigns = false,
+      -- },
       diagnostic = {
-        custom_msg = "■ Diagnostics",
-        custom_fix = " Fix",
+        on_insert = false,
         keys = {
           exec_action = "<cr>",
         },
@@ -32,40 +31,9 @@ return {
       ui = {
         border = "rounded",
         code_action = "",
-        -- catppuccin
-        -- colors = require("catppuccin.groups.integrations.lsp_saga").custom_colors(),
-        -- kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
+        kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
+        -- winblend = 0.5,  -- nvim v0.9.0
       },
     })
-
-    -- ---- Highlights
-    -- -- https://github.com/glepnir/lspsaga.nvim/blob/main/lua/lspsaga/highlight.lua
-    -- local hi = require("config.highlights").hi
-    -- local colors = require("config.highlights").colors
-    --
-    -- -- General
-    -- hi.SagaBorder = { fg = colors.blue }
-    -- hi.SagaCollapse = { fg = colors.red }
-    -- hi.SagaExpand = { fg = colors.red }
-    --
-    -- -- Code Action
-    -- hi.ActionPreviewNormal = { link = "Comment" }
-    -- hi.CodeActionNumber = { fg = colors.green }
-    -- hi.CodeActionText = { fg = colors.orange }
-    -- hi.LspSagaLightBulb = { fg = colors.yellow }
-    --
-    -- -- Diagnostic
-    -- hi.DiagnosticSource = { link = "Comment" }
-    --
-    -- -- Finder
-    -- hi.FinderIcon = { link = "FinderType" }
-    -- hi.FinderSelection = { fg = colors.blue }
-    -- hi.FinderSpinner = { link = "FinderSpinnerTitle" }
-    -- hi.FinderSpinnerTitle = { fg = colors.blue }
-    -- hi.FinderType = { fg = colors.yellow }
-    -- hi.FinderVirtText = { link = "Comment" }
-    --
-    -- -- Rename
-    -- hi.RenameNormal = { fg = colors.cyan }
   end,
 }
