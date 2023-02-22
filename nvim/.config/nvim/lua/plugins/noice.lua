96a123f3a65feb8e51716f000c51d2b61f0f9dcf
David Marchante <davidmarchan@gmail.com>
2023-02-21 20:40:32 -0500

    [vim] Disable some stuff from Noice that Lspsaga already controls

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -14,12 +14,9 @@ return {
   config = function()
     require("noice").setup({
       lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
+        -- Controlled by lspsaga
+        hover = { enabled = false },
+        signature = { enabled = false },
       },
       -- you can enable a preset for easier configuration
       presets = {
