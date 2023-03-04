d643ad1c43de0f57c1e56a176cdcbde395daafcf
David Marchante <davidmarchan@gmail.com>
2023-03-04 16:18:15 -0500

    [vim] Fix issue with zen-mode not able to hide statusline with cmdheight=0

--- nvim/.config/nvim/lua/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -4,20 +4,34 @@ return {
   keys = {
     { "<leader>z", "<cmd>ZenMode<cr>" },
   },
-  opts = {
-    window = {
-      backdrop = 1,
-      height = 0.9,
-      width = 0.9,
-      options = {
-        -- vim.wo options
-        cursorcolumn = false,
-        cursorline = false,
-        list = false,
-        number = false,
-        relativenumber = false,
-        signcolumn = "no",
+  config = function()
+    require("zen-mode").setup({
+      window = {
+        backdrop = 1,
+        height = 0.9,
+        width = 0.9,
+        options = {
+          -- vim.wo options
+          cursorcolumn = false,
+          cursorline = false,
+          list = false,
+          number = false,
+          relativenumber = false,
+          signcolumn = "no",
+        },
       },
-    },
-  },
+      on_open = function(win)
+        require("barbecue.ui").toggle(false)
+
+        -- There's a bug if cmdheight = 0 that zen-mode can't hide the statusline
+        -- https://github.com/folke/zen-mode.nvim/issues/69
+        require("lualine").hide({})
+        vim.o.statusline = " "
+      end,
+      on_close = function()
+        require("barbecue.ui").toggle(true)
+        require("lualine").hide({ unhide = true })
+      end,
+    })
+  end,
 }
