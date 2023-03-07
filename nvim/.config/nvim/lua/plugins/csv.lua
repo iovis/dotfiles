64f4db53f5db5799741b228304b22d79c156a7f6
David Marchante <davidmarchan@gmail.com>
2023-03-06 22:23:32 -0500

    [vim] Remove highlights for CSV

--- nvim/.config/nvim/lua/plugins/csv.lua
+++ nvim/.config/nvim/lua/plugins/csv.lua
@@ -1,11 +1,4 @@
 return {
   "chrisbra/csv.vim",
   ft = "csv",
-  config = function()
-    local hi = require("config.highlights").hi
-    local c = require("config.highlights").colors
-
-    hi.CSVColumnEven = { bg = c.gray3 }
-    hi.CSVColumnOdd = {}
-  end,
 }
