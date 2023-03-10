0bdd673f052b2bb823bbcd64935b82c2dd873621
David Marchante <davidmarchan@gmail.com>
2023-03-10 10:41:44 -0500

    [fish] Add bundler abbrs

--- fish/.config/fish/abbrs.fish
+++ fish/.config/fish/abbrs.fish
@@ -1,13 +1,19 @@
+## bundler
+abbr be   bundle exec
+abbr bi   bundle install
+abbr bout bundle outdated
+abbr bu   bundle update
+
 ## directories
-abbr -- -   'cd -'
-abbr ...    'cd ../..'
-abbr ....   'cd ../../..'
+abbr -- -  'cd -'
+abbr ...   'cd ../..'
+abbr ....  'cd ../../..'
 
 ## ruby
-abbr RED RAILS_ENV=development
-abbr REP RAILS_ENV=production
-abbr REQ RAILS_ENV=qa
-abbr RET RAILS_ENV=test
+abbr RED  RAILS_ENV=development
+abbr REP  RAILS_ENV=production
+abbr REQ  RAILS_ENV=qa
+abbr RET  RAILS_ENV=test
 
 ## rust
 abbr RB RUST_BACKTRACE=1
