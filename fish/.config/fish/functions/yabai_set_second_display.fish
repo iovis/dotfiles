6011a9a0b06d85763340d65c659286cde1b683db
David Marchante <davidmarchan@gmail.com>
2023-03-16 09:58:10 -0400

    [fish] Update yabai scripts

--- fish/.config/fish/functions/yabai_set_second_display.fish
+++ fish/.config/fish/functions/yabai_set_second_display.fish
@@ -1,5 +1,8 @@
 function yabai_set_second_display
     open -a Fantastical
+    if test (yabai -m query --windows --window | jq '."is-floating"') = true
+        yabai -m window --toggle float
+    end
     yabai -m window --display 2
 
     open -a Music
@@ -7,6 +10,9 @@ function yabai_set_second_display
     yabai -m window --grid 10:3:0:10:1:1
 
     open -a Slack
+    if test (yabai -m query --windows --window | jq '."is-floating"') = true
+        yabai -m window --toggle float
+    end
     yabai -m window --display 2
     open -a Slack
     yabai -m window --swap east
