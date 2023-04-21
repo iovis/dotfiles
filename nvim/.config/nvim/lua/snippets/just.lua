ed3e2af377294e2fd54fad675603596d0065f32d
David Marchante <davidmarchan@gmail.com>
2023-04-21 12:48:01 -0400

    [vim] Add just snippets

---
+++ nvim/.config/nvim/lua/snippets/just.lua
@@ -0,0 +1,44 @@
+return {
+  s(
+    "main",
+    fmt(
+      [[
+        # set dotenv-load  # Uncomment to load .env
+
+        default: {}
+
+        alias l := list
+        # lists available tasks
+        list:
+          @just --list
+      ]],
+      {
+        i(1, "list"),
+      }
+    ),
+    { condition = conds.line_begin }
+  ),
+  s(
+    "a",
+    fmt("alias {} := {}", {
+      i(1, "name"),
+      i(2, "task"),
+    }),
+    { condition = conds.line_begin }
+  ),
+  s(
+    "t",
+    fmt(
+      [[
+        {}: {}
+          {}
+      ]],
+      {
+        i(1, "name"),
+        i(2),
+        i(3),
+      }
+    ),
+    { condition = conds.line_begin }
+  ),
+}
