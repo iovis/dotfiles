4f637892a8536db4bed502de6e71d6f5d98f9952
David Marchante <davidmarchan@gmail.com>
2023-04-26 19:49:25 -0400

    [vim] Clean up some bindings

--- nvim/.config/nvim/lua/config/keymap.lua
+++ nvim/.config/nvim/lua/config/keymap.lua
@@ -60,10 +60,10 @@ vim.keymap.set("n", "<leader>E", ":e<space><c-r>=fnameescape(expand('%:h')).'/'<
 vim.keymap.set("n", "<leader>W", ":saveas <c-r>=fnameescape(expand('%:h')).'/'<cr>")
 
 -- Editing
-vim.keymap.set("n", "<m-j>", ":m+<cr>==")
-vim.keymap.set("n", "<m-k>", ":m-2<cr>==")
-vim.keymap.set("x", "<m-j>", ":m'>+<cr>`<my`>mzgv=gv`yo`z")
-vim.keymap.set("x", "<m-k>", ":m'<-2<cr>`>my`<mzgv=gv`yo`z")
+vim.keymap.set("n", "<m-j>", "<cmd>m+<cr>==")
+vim.keymap.set("n", "<m-k>", "<cmd>m-2<cr>==")
+vim.keymap.set("x", "<m-j>", ":m'>+<cr>`<my`>mzgv=gv`yo`z", { silent = true })
+vim.keymap.set("x", "<m-k>", ":m'<-2<cr>`>my`<mzgv=gv`yo`z", { silent = true })
 
 vim.keymap.set("n", "<m-o>", "mzo<esc>`z")
 vim.keymap.set("n", "<m-O>", "mzO<esc>`z")
@@ -87,8 +87,12 @@ vim.keymap.set("n", "Q", "@q")
 vim.keymap.set("x", "Q", ":norm @q<cr>", { silent = true })
 
 -- Movement
-vim.keymap.set({ "n", "x" }, "j", "(v:count == 0 ? 'gj' : 'j')", { expr = true })
-vim.keymap.set({ "n", "x" }, "k", "(v:count == 0 ? 'gk' : 'k')", { expr = true })
+vim.keymap.set({ "n", "x" }, "j", function()
+  return vim.v.count == 0 and "gj" or "j"
+end, { expr = true })
+vim.keymap.set({ "n", "x" }, "k", function()
+  return vim.v.count == 0 and "gk" or "k"
+end, { expr = true })
 
 vim.keymap.set({ "n", "x", "o" }, "H", "g^")
 vim.keymap.set({ "n", "x", "o" }, "L", "g$")
@@ -130,6 +134,9 @@ vim.keymap.set("n", "P", [[P<cmd>execute ":silent normal! `[v`]="<cr>]])
 vim.keymap.set("n", "gp", "p")
 vim.keymap.set("n", "gP", "P")
 
+vim.keymap.set("x", "p", "P") -- don't copy selected text to unnamed register
+vim.keymap.set("x", "P", "p")
+
 vim.keymap.set({ "n", "x" }, "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+y$')
 vim.keymap.set({ "n", "x" }, "<leader>d", '"+d')
@@ -157,7 +164,7 @@ vim.keymap.set("n", "<leader><right>", "<cmd>lnfile<cr>")
 -- Repeat
 vim.keymap.set("n", "<leader>.", "@:")
 vim.keymap.set("n", "<leader>,", "@@")
-vim.keymap.set("x", ".", ":normal .<cr>")
+vim.keymap.set("x", ".", ":normal .<cr>", { silent = true })
 
 -- Replace
 vim.keymap.set("n", "R", "ciw<c-r>0<esc>")
@@ -170,7 +177,7 @@ vim.keymap.set({ "n", "x" }, "Ñ", "?")
 vim.keymap.set({ "n", "x" }, "<leader>ñ", "<cmd>nohlsearch<cr>")
 
 vim.keymap.set("n", "*", [[:let @/= '\<' . expand('<cword>') . '\>' <bar> set hls <cr>]], { silent = true })
-vim.keymap.set("x", "*", [[y:let @/= '\V' . escape(@@, '/\') <bar> normal! /<cr>]], { silent = true })
+vim.keymap.set("x", "*", [[y:let @/= '\V' . escape(@@, '/\') <bar> set hls <bar> normal! /<cr>]], { silent = true })
 
 -- Tabs
 vim.keymap.set("n", "<leader>T", "<cmd>tabnew<cr>")
@@ -182,11 +189,19 @@ vim.keymap.set("n", "+q", "<cmd>tabonly<cr>")
 vim.keymap.set("n", "t", "<c-]>")
 vim.keymap.set("n", "T", "g]")
 
----- Quick File access (TODO: do I need this anymore?)
+---- Quick File access
 vim.keymap.set("n", "<leader>u", "<nop>")
 
+-- Config files
 vim.keymap.set("n", "<leader>uf", "<cmd>EditFtplugin<cr>")
 vim.keymap.set("n", "<leader>uu", "<cmd>e! $MYVIMRC<cr>")
+
+-- Notes
+vim.keymap.set("n", "<leader>un", ":e! notes/index.md<cr>")
+vim.keymap.set("n", "<leader>N", function()
+  local note = string.format("notes/%s.md", vim.fn.strftime("%F"))
+  vim.cmd.edit(note)
+end, { desc = "Open daily note" })
 
 -- nnoremap <silent> <leader>us :so $MYVIMRC<cr>:echo 'vimrc sourced'<cr>
 --
@@ -200,22 +215,22 @@ vim.keymap.set("n", "<leader>uu", "<cmd>e! $MYVIMRC<cr>")
 -- nnoremap <silent> <leader>uz :e! $FDOTDIR/config.fish<cr>
 
 ---- Toggle Settings
-vim.keymap.set("n", "yo", "<cmd>set number! relativenumber! cursorline!<cr>")
+vim.keymap.set("n", "yo,", ":set number! relativenumber! cursorline!<cr>")
 
-vim.keymap.set("n", "yoc", "<cmd>set cursorline!<cr>")
+vim.keymap.set("n", "yoc", ":set cursorline!<cr>")
 vim.keymap.set("n", "yod", ":<c-r>=&diff ? 'diffoff' : 'diffthis'<cr><cr>")
-vim.keymap.set("n", "yoh", "<cmd>set hlsearch!<cr>")
-vim.keymap.set("n", "yol", "<cmd>set list!<cr>")
-vim.keymap.set("n", "yon", "<cmd>set number!<cr>")
-vim.keymap.set("n", "yop", "<cmd>set paste!<cr>")
-vim.keymap.set("n", "yor", "<cmd>set relativenumber!<cr>")
+vim.keymap.set("n", "yoh", ":set hlsearch!<cr>")
+vim.keymap.set("n", "yol", ":set list!<cr>")
+vim.keymap.set("n", "yon", ":set number!<cr>")
+vim.keymap.set("n", "yop", ":set paste!<cr>")
+vim.keymap.set("n", "yor", ":set relativenumber!<cr>")
 
-vim.keymap.set("n", "yos", "<cmd>setlocal spell! spelllang=en_us<cr>")
-vim.keymap.set("n", "yow", "<cmd>setlocal wrap!<cr>")
+vim.keymap.set("n", "yos", ":setlocal spell! spelllang=en_us<cr>")
+vim.keymap.set("n", "yow", ":setlocal wrap!<cr>")
 
 ---- Misc
 vim.keymap.set("n", "<Space>", "<Nop>")
--- vim.keymap.set("n", "q:", ":") -- If you enable, then you have to wait when finishing macros
+-- vim.keymap.set("n", "q:", ":") -- If you enable this, then you have to wait when finishing macros
 
 vim.keymap.set("n", "+<cr>", "<cmd>so $VIMRUNTIME/syntax/hitest.vim<cr>")
 
@@ -223,14 +238,12 @@ vim.keymap.set("n", "_", ":R=")
 vim.keymap.set("n", "<leader>P", ":=")
 vim.keymap.set("n", "<leader>M", "<cmd>R! messages<cr>")
 
----- Notes
-vim.keymap.set("n", "<leader>n", ":e notes/index.md<cr>", { silent = true })
-vim.keymap.set("n", "<leader>N", [[:execute "e notes/" . strftime('%F') . ".md"<cr>]], { silent = true })
-
 ---- Global substitutions
 vim.keymap.set({ "n", "x" }, "+g", ":g//<left>")
-vim.keymap.set({ "n", "x" }, "+l", ':luado return string.format("%s", line)')
 vim.keymap.set({ "n", "x" }, "+v", ":v//<left>")
+vim.keymap.set({ "n", "x" }, "+l", function()
+  return ':luado return string.format("-- %s", line)' .. ("<left>"):rep(10)
+end, { expr = true })
 
 ---- Tmux quick switching
 vim.keymap.set("n", "++", "<cmd>TmuxNewSession<cr>")
