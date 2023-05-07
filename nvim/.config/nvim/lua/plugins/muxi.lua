8325f5cdb305910fe21cf1167614d62fdaeaa7a8
David Marchante <davidmarchan@gmail.com>
2023-05-07 15:40:36 -0400

    [vim] Adopt muxi.nvim formally

--- nvim/.config/nvim/lua/plugins/muxi.lua
+++ nvim/.config/nvim/lua/plugins/muxi.lua
@@ -5,38 +5,45 @@ return {
   event = "VeryLazy",
   keys = {},
   init = function()
-    local keys = { "h", "j", "k", "l" }
+    local keys = { "h", "j", "k", "l", "ñ" }
 
     for _, key in ipairs(keys) do
-      vim.keymap.set("n", "<leader>g" .. key:upper(), function()
+      vim.keymap.set("n", "g" .. key:upper(), function()
         require("muxi").add(key)
+        vim.notify("Added current file to " .. key)
       end, { desc = "[muxi] Add session to " .. key })
 
-      vim.keymap.set("n", "<leader>g" .. key, function()
+      vim.keymap.set("n", "g" .. key, function()
         require("muxi").go_to(key)
       end, { desc = "[muxi] go to session " .. key })
     end
 
-    vim.keymap.set("n", "<leader>gs", require("muxi").go_to_prompt)
-    vim.keymap.set("n", "<leader>gD", require("muxi").delete_prompt)
+    -- ñ:upper() doesn't work great
+    vim.keymap.set("n", "gÑ", function()
+      require("muxi").add("ñ")
+      vim.notify("Added current file to ñ")
+    end, { desc = "[muxi] Add session to ñ" })
+
+    vim.keymap.set("n", "<leader>gs", require("muxi").go_to_prompt, { desc = "[muxi] Interactive go to" })
+    vim.keymap.set("n", "<leader>gd", require("muxi").delete_prompt, { desc = "[muxi] Interactive delete" })
 
     vim.keymap.set("n", "<leader>g-", function()
       require("muxi").clear_all()
       vim.notify("Cleared current session")
-    end)
+    end, { desc = "[muxi] Clear current workspace" })
 
     ---Testing-------------------------------------
     vim.keymap.set("n", "<leader>ge", function()
       vim.cmd("R! =require('muxi').sessions")
       vim.cmd("se ft=lua")
-    end)
+    end, { desc = "[muxi] Inspect current workspace" })
 
     vim.keymap.set("n", "<leader>gm", function()
       local muxi_path = require("muxi").config.path
 
       vim.cmd.split(muxi_path)
       vim.keymap.set("n", "q", "<cmd>q!<cr>", { buffer = true })
-    end)
+    end, { desc = "[muxi] Open storage" })
 
     vim.keymap.set("n", "<leader>gr", function()
       for name, _ in pairs(package.loaded) do
@@ -48,7 +55,7 @@ return {
       print("muxi reloaded")
 
       require("muxi").setup({})
-    end)
+    end, { desc = "[muxi] Reload muxi" })
   end,
   config = function()
     require("muxi").setup({})
