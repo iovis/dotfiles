1d2a64a1908c9fdc4a8693dc0976dca022eee417
David Marchante <davidmarchan@gmail.com>
2023-05-10 17:03:26 -0400

    [vim] Not sure why I decided to manually map what are already the default bindings

--- nvim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,15 +1,7 @@
 return {
   "christoomey/vim-tmux-navigator",
   event = "VeryLazy",
-  keys = {
-    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" }, -- c-ç
-  },
   init = function()
     vim.g.tmux_navigator_save_on_switch = 2
-    vim.g.tmux_navigator_no_mappings = 1
   end,
 }
