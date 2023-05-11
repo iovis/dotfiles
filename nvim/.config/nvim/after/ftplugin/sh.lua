9bfa904e7578584561f63d29971b331a5b490386
David Marchante <davidmarchan@gmail.com>
2023-05-11 16:26:28 -0400

    [yabai] Change how yabai is restarted

--- nvim/.config/nvim/after/ftplugin/sh.lua
+++ nvim/.config/nvim/after/ftplugin/sh.lua
@@ -1,4 +1,4 @@
 ---- yabai
 if vim.fn.expand("%"):match("yabai/") then
-  vim.keymap.set("n", "<leader>so", "<cmd>TuxBg! brew services restart yabai<cr>", { buffer = true })
+  vim.keymap.set("n", "<leader>so", "<cmd>TuxBg! yabai --restart-service<cr>", { buffer = true })
 end
