d46668cac9c77ae6691109ae004415d7b7c8d640
David Marchante <davidmarchan@gmail.com>
2023-05-26 17:40:33 -0400

    [vim] Put a reminder of why I don't allow default mappings for tmux-navigator

--- nvim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,7 +1,17 @@
 return {
   "christoomey/vim-tmux-navigator",
   event = "VeryLazy",
+  keys = {
+    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" }, -- c-ç
+  },
   init = function()
     vim.g.tmux_navigator_save_on_switch = 2
+
+    -- Otherwise they'll map in select mode and mess up snippets
+    vim.g.tmux_navigator_no_mappings = 1
   end,
 }
