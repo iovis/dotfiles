eddadce20ec9238c06252dda15bc710f6e8ad775
David Marchante <davidmarchan@gmail.com>
2023-06-22 20:09:11 -0400

    feat(vim): Add snippets for conventional commits

--- nvim/.config/nvim/lua/snippets/gitcommit.lua
+++ nvim/.config/nvim/lua/snippets/gitcommit.lua
@@ -1,5 +1,15 @@
 local u = require("config.utils")
 
+local conventional_commit = function()
+  return sn(
+    nil,
+    c(1, {
+      sn(nil, fmt("({}): {}", { r(1, "scope", i(1)), r(2, "message", i(2)) })),
+      sn(nil, fmt("({})!: {}", { r(1, "scope", i(1)), r(2, "message", i(2)) })),
+    })
+  )
+end
+
 local get_branch = function()
   return u.system("git rev-parse --abbrev-ref HEAD")
 end
@@ -40,21 +50,58 @@ local get_pr_title = function()
 end
 
 return {
-  -- Labels
-  s("brew", t("[brew] ")),
-  s("fish", t("[fish] ")),
-  s("gem", t("[gem] ")),
-  s("git", t("[git] ")),
-  s("muxi", t("[muxi] ")),
-  s("npm", t("[npm] ")),
-  s("pip", t("[pip] ")),
-  s("rtx", t("[rtx] ")),
-  s("skhd", t("[skhd] ")),
-  s("tmux", t("[tmux] ")),
-  s("vim", t("[vim] ")),
-  s("wip", t("[WIP] ")),
-  s("yabai", t("[yabai] ")),
-  s("zsh", t("[zsh] ")),
+  -- Conventional Commits
+  s(
+    { trig = "build", dscr = "Changes that affect the build system or external dependencies" },
+    fmt("build{}", d(1, conventional_commit)),
+    { condition = conds.line_begin }
+  ),
+  s(
+    { trig = "chore", dscr = "Other changes that dont modify src or test files" },
+    fmt("chore{}", d(1, conventional_commit)),
+    { condition = conds.line_begin }
+  ),
+  s(
+    { trig = "ci", dscr = "Changes to our CI configuration files and scripts" },
+    fmt("ci{}", d(1, conventional_commit)),
+    { condition = conds.line_begin }
+  ),
+  s(
+    { trig = "docs", dscr = "Documentation only changes" },
+    fmt("docs{}", d(1, conventional_commit)),
+    { condition = conds.line_begin }
+  ),
+  s(
+    { trig = "feat", dscr = "A new feature" },
+    fmt("feat{}", d(1, conventional_commit)),
+    { condition = conds.line_begin }
+  ),
+  s({ trig = "fix", dscr = "A bug fix" }, fmt("fix{}", d(1, conventional_commit)), { condition = conds.line_begin }),
+  s(
+    { trig = "perf", dscr = "A code change that improves performance" },
+    fmt("perf{}", d(1, conventional_commit)),
+    { condition = conds.line_begin }
+  ),
+  s(
+    { trig = "refactor", dscr = "A code change that neither fixes a bug nor adds a feature" },
+    fmt("refactor{}", d(1, conventional_commit)),
+    { condition = conds.line_begin }
+  ),
+  s(
+    { trig = "revert", dscr = "Reverts a previous commit" },
+    fmt("revert{}", d(1, conventional_commit)),
+    { condition = conds.line_begin }
+  ),
+  s(
+    { trig = "style", dscr = "Changes that do not affect the meaning of the code" },
+    fmt("style{}", d(1, conventional_commit)),
+    { condition = conds.line_begin }
+  ),
+  s(
+    { trig = "test", dscr = "Adding missing tests or correcting existing tests" },
+    fmt("test{}", d(1, conventional_commit)),
+    { condition = conds.line_begin }
+  ),
   -- Templates
   s(
     "master",
@@ -75,7 +122,8 @@ return {
         i(4),
         i(0),
       }
-    )
+    ),
+    { condition = conds.line_begin }
   ),
   s(
     "qa",
@@ -97,7 +145,8 @@ return {
         changes = i(4),
         test = i(5),
       }
-    )
+    ),
+    { condition = conds.line_begin }
   ),
   s(
     "coauthor",
@@ -106,6 +155,7 @@ return {
         Co-authored-by: {name} <{email}>
       ]],
       { name = i(1), email = i(0) }
-    )
+    ),
+    { condition = conds.line_begin }
   ),
 }
