82211132f3edb9a69b6dcd4789a73ab9e965ebb2
David Marchante <davidmarchan@gmail.com>
2023-06-24 18:32:26 -0400

    feat(vim): don't use <tab> in diffview.nvim

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -18,13 +18,25 @@ return {
     require("diffview").setup({
       keymaps = {
         view = {
+          ["<tab>"] = false,
+          ["<s-tab>"] = false,
+          { "n", "<down>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
+          { "n", "<up>", actions.select_prev_entry, { desc = "Open the diff for the previous file" } },
           { "n", "<leader>k", actions.toggle_files, { desc = "Toggle the file panel." } },
         },
         file_panel = {
+          ["<tab>"] = false,
+          ["<s-tab>"] = false,
+          { "n", "<down>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
+          { "n", "<up>", actions.select_prev_entry, { desc = "Open the diff for the previous file" } },
           { "n", "<leader>k", actions.toggle_files, { desc = "Toggle the file panel." } },
           { "n", "r", actions.cycle_layout, { desc = "Cycle available layouts" } },
         },
         file_history_panel = {
+          ["<tab>"] = false,
+          ["<s-tab>"] = false,
+          { "n", "<down>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
+          { "n", "<up>", actions.select_prev_entry, { desc = "Open the diff for the previous file" } },
           { "n", "<leader>k", actions.toggle_files, { desc = "Toggle the file panel." } },
           { "n", "r", actions.cycle_layout, { desc = "Cycle available layouts" } },
         },
