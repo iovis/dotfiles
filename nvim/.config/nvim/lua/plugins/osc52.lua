972b51823eb3177404fa4156bac7b703b6745e64
David Marchante <davidmarchan@gmail.com>
2023-07-27 11:07:01 -0400

    feat(vim.osc52): Don't setup if condition instead of using `cond`
    Otherwise the different machines get messed up when syncing packages

--- nvim/.config/nvim/lua/plugins/osc52.lua
+++ nvim/.config/nvim/lua/plugins/osc52.lua
@@ -2,9 +2,14 @@ return {
   "ojroques/nvim-osc52",
   event = "VeryLazy",
   -- cond = function()
+  --   -- Bit awkward to share the lazy.lock this way
   --   return vim.env.SSH_CLIENT ~= nil
   -- end,
   config = function()
+    if not vim.env.SSH_CLIENT then
+      return
+    end
+
     require("osc52").setup({})
 
     local osc52_augroup = vim.api.nvim_create_augroup("osc52_yank", { clear = true })
