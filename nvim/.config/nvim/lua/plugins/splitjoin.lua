ae6b0aeb6203d5ad7badc13afd148ad3743cbc35
David Marchante <davidmarchan@gmail.com>
2023-08-07 11:39:34 -0400

    feat(nvim.treesj): Fall back to splitjoin.vim for Ruby classes/modules
    https://github.com/Wansmer/treesj/issues/81

--- nvim/.config/nvim/lua/plugins/splitjoin.lua
+++
@@ -1,9 +0,0 @@
--- Fallback for treesj for Ruby modules
-return {
-  "AndrewRadev/splitjoin.vim",
-  lazy = false,
-  init = function()
-    vim.g.splitjoin_join_mapping = "+J"
-    vim.g.splitjoin_split_mapping = "+S"
-  end,
-}
--- nvim/.config/nvim/lua/plugins/treesj.lua
+++ nvim/.config/nvim/lua/plugins/treesj.lua
@@ -4,9 +4,33 @@ return {
     { "+j", ":TSJJoin<cr>", silent = true },
     { "+s", ":TSJSplit<cr>", silent = true },
   },
+  dependencies = {
+    "AndrewRadev/splitjoin.vim",
+  },
   config = function()
     require("treesj").setup({
       use_default_keymaps = false,
+      langs = {
+        -- Fallback to splitjoin.vim for modules/classes
+        ruby = {
+          module = {
+            both = {
+              no_format_with = {}, -- Need to avoid 'no format with comment'
+              fallback = function(_)
+                vim.cmd("SplitjoinJoin")
+              end,
+            },
+          },
+          class = {
+            both = {
+              no_format_with = {},
+              fallback = function(_)
+                vim.cmd("SplitjoinSplit")
+              end,
+            },
+          },
+        },
+      },
     })
   end,
 }
