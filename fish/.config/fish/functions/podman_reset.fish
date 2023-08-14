56952fbfce290aeb50dd359a23a432fcf929a946
David Marchante <davidmarchan@gmail.com>
2023-08-14 10:08:56 -0400

    feat(fish): Don't ask for confirmation resetting the podman machine

--- fish/.config/fish/functions/podman_reset.fish
+++ fish/.config/fish/functions/podman_reset.fish
@@ -1,3 +1,3 @@
 function podman_reset --wraps='podman machine stop && podman machine rm podman-machine-default && podman machine init --cpus 2 --memory 2048 --disk-size 40 && podman machine start' --description 'alias podman_reset podman machine stop && podman machine rm podman-machine-default && podman machine init --cpus 2 --memory 2048 --disk-size 40 && podman machine start'
-    podman machine stop && podman machine rm podman-machine-default && podman machine init --cpus 2 --memory 2048 --disk-size 40 && podman machine start $argv
+    podman machine stop && podman machine rm -f podman-machine-default && podman machine init --cpus 2 --memory 2048 --disk-size 40 && podman machine start $argv
 end
