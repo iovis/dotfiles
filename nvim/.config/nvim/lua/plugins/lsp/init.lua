70d74556a45a3f4e7b7548b57b23be9b99d75e96
David Marchante <davidmarchan@gmail.com>
2023-09-06 15:58:23 -0400

    refactor(lsp): Make LSP config consistent with lazy

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,4 +1,4 @@
-local M = {
+return {
   "neovim/nvim-lspconfig",
   dependencies = {
     "b0o/schemastore.nvim",
@@ -12,101 +12,99 @@ local M = {
       },
     },
   },
-}
+  config = function()
+    ---- Initialize servers
+    local lsp_servers = {
+      -- "angularls",
+      "bashls",
+      "clangd",
+      "cssls",
+      "dockerls",
+      "emmet_language_server",
+      "gopls",
+      "html",
+      "jsonls",
+      "lua_ls",
+      "marksman",
+      "pyright",
+      "rust_analyzer",
+      "solargraph",
+      -- "sorbet",
+      "svelte",
+      "taplo",
+      "tsserver",
+      -- "vimls",
+      "zls",
+    }
 
-function M.config()
-  ---- Initialize servers
-  local lsp_servers = {
-    -- "angularls",
-    "bashls",
-    "clangd",
-    "cssls",
-    "dockerls",
-    "emmet_language_server",
-    "gopls",
-    "html",
-    "jsonls",
-    "lua_ls",
-    "marksman",
-    "pyright",
-    "rust_analyzer",
-    "solargraph",
-    -- "sorbet",
-    "svelte",
-    "taplo",
-    "tsserver",
-    -- "vimls",
-    "zls",
-  }
+    require("neodev").setup({})
 
-  require("neodev").setup({})
-
-  require("mason").setup({
-    ui = {
-      border = "rounded",
-      keymaps = {
-        apply_language_filter = "Ñ",
+    require("mason").setup({
+      ui = {
+        border = "rounded",
+        keymaps = {
+          apply_language_filter = "Ñ",
+        },
       },
-    },
-  })
+    })
 
-  require("mason-lspconfig").setup({})
+    require("mason-lspconfig").setup({})
 
-  ---- LSP Settings files loading
-  local config = require("plugins.lsp.config")
-  local scan = require("plenary.scandir")
+    ---- LSP Settings files loading
+    local lspconfig = require("lspconfig")
+    local config = require("plugins.lsp.config")
+    local scan = require("plenary.scandir")
 
-  local lsp_settings_path = vim.fn.stdpath("config") .. "/lua/plugins/lsp/settings"
-  local lsp_settings_files = scan.scan_dir(lsp_settings_path, { depth = 0 })
+    local lsp_settings_path = vim.fn.stdpath("config") .. "/lua/plugins/lsp/settings"
+    local lsp_settings_files = scan.scan_dir(lsp_settings_path, { depth = 0 })
 
-  local lsp_settings = {}
+    local lsp_settings = {}
 
-  for _, file in ipairs(lsp_settings_files) do
-    local language = file:match("([^/]+).lua$")
+    for _, file in ipairs(lsp_settings_files) do
+      local language = file:match("([^/]+).lua$")
 
-    lsp_settings[language] = require("plugins.lsp.settings." .. language)
-  end
+      lsp_settings[language] = require("plugins.lsp.settings." .. language)
+    end
 
-  ---- LSP Server init
-  for _, server in ipairs(lsp_servers) do
+    ---- LSP Server init
+    for _, server in ipairs(lsp_servers) do
+      local opts = {
+        on_attach = config.on_attach,
+        capabilities = config.capabilities,
+      }
+
+      -- Autoloading of plugins.lsp.settings.*
+      if lsp_settings[server] then
+        opts = vim.tbl_deep_extend("force", lsp_settings[server], opts)
+      end
+
+      if server == "clangd" then
+        -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428#issuecomment-997226723
+        opts.capabilities.offsetEncoding = { "utf-16" }
+      end
+
+      lspconfig[server].setup(opts)
+    end
+
+    ---- Rust
+    -- DAP config
+    -- local extension_path = require("mason-registry").get_package("codelldb"):get_install_path()
+    -- local codelldb_path = extension_path .. "/extension/adapter/codelldb"
+    -- local liblldb_path = extension_path .. "/extension/lldb/lib/liblldb.dylib"
+
     local opts = {
       on_attach = config.on_attach,
       capabilities = config.capabilities,
+      standalone = false,
     }
 
-    -- Autoloading of plugins.lsp.settings.*
-    if lsp_settings[server] then
-      opts = vim.tbl_deep_extend("force", lsp_settings[server], opts)
-    end
+    opts = vim.tbl_deep_extend("force", lsp_settings["rust_analyzer"], opts)
 
-    if server == "clangd" then
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428#issuecomment-997226723
-      opts.capabilities.offsetEncoding = { "utf-16" }
-    end
-
-    require("lspconfig")[server].setup(opts)
-  end
-
-  ---- Rust
-  -- DAP config
-  -- local extension_path = require("mason-registry").get_package("codelldb"):get_install_path()
-  -- local codelldb_path = extension_path .. "/extension/adapter/codelldb"
-  -- local liblldb_path = extension_path .. "/extension/lldb/lib/liblldb.dylib"
-
-  local opts = {
-    on_attach = config.on_attach,
-    capabilities = config.capabilities,
-    standalone = false,
-  }
-
-  opts = vim.tbl_deep_extend("force", lsp_settings["rust_analyzer"], opts)
-
-  require("rust-tools").setup({
-    server = opts,
-    -- dap = {
-    --   adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-    -- },
-  })
-end
-
-return M
+    require("rust-tools").setup({
+      server = opts,
+      -- dap = {
+      --   adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+      -- },
+    })
+  end,
+}
