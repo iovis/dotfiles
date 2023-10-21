986c5c10b352f134bd4188de44dd49ba01f67ff1
David Marchante <davidmarchan@gmail.com>
2023-10-21 02:26:03 -0400

    feat(nvim.leap): Add mappings for , and ;

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -3,9 +3,13 @@ return {
   -- enabled = false,
   event = "VeryLazy",
   config = function()
-    require("leap").setup({})
+    local leap = require("leap")
+
+    leap.setup({})
 
     vim.keymap.set({ "n", "x", "o" }, "f", "<Plug>(leap-forward-to)")
     vim.keymap.set({ "n", "x", "o" }, "F", "<Plug>(leap-backward-to)")
+
+    leap.add_repeat_mappings(";", ",")
   end,
 }
