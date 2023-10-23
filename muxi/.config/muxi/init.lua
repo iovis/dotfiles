dac8e84192d217f4dda32bfa16672cf8a5f959db
David Marchante <davidmarchan@gmail.com>
2023-10-22 23:08:42 -0400

    feat(muxi): Update to muxi v2.0

---
+++ muxi/.config/muxi/init.lua
@@ -0,0 +1,29 @@
+-- TODO: Add EmmyLua annotations
+
+-- TODO: Add to muxi?
+local inspect = require("inspect")
+
+local nvim_opts = "+ZenMode -c 'nmap <silent> q :wqa<cr>'"
+
+-- muxi.uppercase_overrides = true
+muxi.bindings = {
+  ["-"] = {
+    command = "muxi fzf",
+  },
+  c = {
+    popup = { title = " config ", height = "66%" },
+    command = ("muxi config edit -- %s"):format(nvim_opts),
+  },
+  e = {
+    popup = { title = " sessions ", height = "66%" },
+    command = ("muxi sessions edit -- %s"):format(nvim_opts),
+  },
+  ["ñ"] = {
+    command = "muxi sessions switch ñ",
+  },
+  ["Ñ"] = {
+    command = "muxi sessions set ñ && tmux display 'bound current session to ñ'",
+  },
+}
+
+-- print(inspect(muxi))
