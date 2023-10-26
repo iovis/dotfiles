02e30d6f869fc8c539b39a57a73304bf41452594
David Marchante <davidmarchan@gmail.com>
2023-10-26 08:26:47 -0400

    feat(hammerspoon): I don't seem to be able to use the Defy
    It's Defying my hands

--- hammerspoon/.hammerspoon/key_overrides.lua
+++ hammerspoon/.hammerspoon/key_overrides.lua
@@ -7,21 +7,21 @@ hs.hotkey.bind({}, "`", function()
 end)
 
 -- TODO: Only enable when using the Defy
-hs.hotkey.bind({ "alt", "shift" }, "+", function()
-  hs.eventtap.keyStrokes("]")
-end)
-
-hs.hotkey.bind({ "alt" }, "+", function()
-  hs.eventtap.keyStrokes("[")
-end)
-
-hs.hotkey.bind({ "alt", "shift" }, "return", function()
-  hs.eventtap.keyStrokes("}")
-end)
-
-hs.hotkey.bind({ "alt" }, "return", function()
-  hs.eventtap.keyStrokes("{")
-end)
+-- hs.hotkey.bind({ "alt", "shift" }, "+", function()
+--   hs.eventtap.keyStrokes("]")
+-- end)
+--
+-- hs.hotkey.bind({ "alt" }, "+", function()
+--   hs.eventtap.keyStrokes("[")
+-- end)
+--
+-- hs.hotkey.bind({ "alt", "shift" }, "return", function()
+--   hs.eventtap.keyStrokes("}")
+-- end)
+--
+-- hs.hotkey.bind({ "alt" }, "return", function()
+--   hs.eventtap.keyStrokes("{")
+-- end)
 
 ---Debug keystrokes
 -- print(hs.inspect(hs.keycodes.map))
