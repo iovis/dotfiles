0d6abb6aecce0ce315b3fd5a3ae578eb70201ef9
David Marchante <davidmarchan@gmail.com>
2023-10-27 17:34:52 -0400

    feat(nvim.ft.c): Move between header and source file

--- nvim/.config/nvim/after/ftplugin/cpp.lua
+++ nvim/.config/nvim/after/ftplugin/cpp.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "s<cr>", "<cmd>Tux just run<cr>", { buffer = true })
 vim.keymap.set("n", "m<cr>", "<cmd>Tux just build<cr>", { buffer = true })
+
+vim.keymap.set("n", "<leader>aa", "<cmd>ClangdSwitchSourceHeader<cr>", { buffer = true })
