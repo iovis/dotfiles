59fd62fc1a0f5fa8f576cd31c3a0b78991386177
David Marchante <davidmarchan@gmail.com>
2023-10-31 23:51:10 -0400

    feat(nvim.ft.c): Update run/build bindings

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -2,8 +2,8 @@
 if vim.fn.expand("%"):match("ext/") then
   vim.keymap.set("n", "m<cr>", "<cmd>Tux bear -- rake compile<cr>", { buffer = true })
 else
-  vim.keymap.set("n", "s<cr>", "<cmd>Tux just run<cr>", { buffer = true })
-  vim.keymap.set("n", "m<cr>", "<cmd>Tux just build<cr>", { buffer = true })
+  vim.keymap.set("n", "s<cr>", "<cmd>Tux bear -- just run<cr>", { buffer = true })
+  vim.keymap.set("n", "m<cr>", "<cmd>Tux bear -- just build<cr>", { buffer = true })
 end
 
 vim.keymap.set("n", "<leader>aa", "<cmd>ClangdSwitchSourceHeader<cr>", { buffer = true })
