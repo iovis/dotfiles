9774493111bef8a59edbacab4a164e441d6efbba
David Marchante <davidmarchan@gmail.com>
2023-11-03 10:41:21 -0400

    feat(muxi): Use current pane's path (muxi v2.1.0)

--- muxi/.config/muxi/init.lua
+++ muxi/.config/muxi/init.lua
@@ -6,6 +6,7 @@ local inspect = require("inspect")
 local nvim_opts = "+ZenMode -c 'nmap <silent> q :wqa<cr>'"
 
 muxi.uppercase_overrides = true
+muxi.use_current_pane_path = true
 muxi.bindings = {
   ["-"] = {
     command = "muxi fzf",
