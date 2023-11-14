14c6c9aea02b24b22620dc00447a6cb96db5cdb5
David Marchante <davidmarchan@gmail.com>
2023-11-13 22:37:17 -0500

    feat(nvim.fidget): Unpin and update to latest
    It recently got a rewrite

--- nvim/.config/nvim/lua/plugins/fidget.lua
+++ nvim/.config/nvim/lua/plugins/fidget.lua
@@ -1,21 +1,13 @@
 return {
   "j-hui/fidget.nvim",
-  tag = "legacy",
   event = "VeryLazy",
   config = function()
     require("fidget").setup({
-      text = {
-        spinner = "dots",
-      },
-      window = {
-        relative = "editor",
-        blend = 0,
-      },
-      sources = {
-        ["null-ls"] = {
-          ignore = true,
-        },
-      },
+      notification = {
+        window = {
+          winblend = 0,
+        }
+      }
     })
   end,
 }
