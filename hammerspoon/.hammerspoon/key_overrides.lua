4f2fec708e93076edd79194bb61fdebdb14f5543
David Marchante <davidmarchan@gmail.com>
2023-11-18 12:14:52 -0500

    feat(hammerspoon): clean up

--- hammerspoon/.hammerspoon/key_overrides.lua
+++ hammerspoon/.hammerspoon/key_overrides.lua
@@ -6,28 +6,34 @@ hs.hotkey.bind({}, "`", function()
   hs.eventtap.keyStrokes("`")
 end)
 
--- TODO: Only enable when using the Defy
--- hs.hotkey.bind({ "alt", "shift" }, "+", function()
---   hs.eventtap.keyStrokes("]")
--- end)
---
--- hs.hotkey.bind({ "alt" }, "+", function()
---   hs.eventtap.keyStrokes("[")
--- end)
---
--- hs.hotkey.bind({ "alt", "shift" }, "return", function()
---   hs.eventtap.keyStrokes("}")
--- end)
---
--- hs.hotkey.bind({ "alt" }, "return", function()
---   hs.eventtap.keyStrokes("{")
--- end)
+local defy = false
+if defy then
+  hs.hotkey.bind({ "alt", "shift" }, "+", function()
+    hs.eventtap.keyStrokes("]")
+  end)
+
+  hs.hotkey.bind({ "alt" }, "+", function()
+    hs.eventtap.keyStrokes("[")
+  end)
+
+  hs.hotkey.bind({ "alt", "shift" }, "return", function()
+    hs.eventtap.keyStrokes("}")
+  end)
+
+  hs.hotkey.bind({ "alt" }, "return", function()
+    hs.eventtap.keyStrokes("{")
+  end)
+end
 
 ---Debug keystrokes
--- print(hs.inspect(hs.keycodes.map))
---
--- tap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
---   print(hs.inspect(event:getRawEventData()))
--- end)
---
--- tap:start()
+local debug = false
+
+if debug then
+  print(hs.inspect(hs.keycodes.map))
+
+  local tap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
+    print(hs.inspect(event:getRawEventData()))
+  end)
+
+  tap:start()
+end
