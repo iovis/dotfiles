65d6f401a5ba23c3c9f1a4343fe657d2423919ad
David Marchante <davidmarchan@gmail.com>
2023-11-19 15:07:52 -0500

    feat(fish.tmux): Rework `tmux list-keys` alias
    Turns out you can pass certain arguments that are interesting on its own

--- fish/.config/fish/functions/tkg.fish
+++
@@ -1,3 +0,0 @@
-function tkg --wraps='tmux list-keys | g' --description 'alias tkg=tmux list-keys | g'
-    tmux list-keys | g $argv
-end
---
+++ fish/.config/fish/functions/tlk.fish
@@ -0,0 +1,3 @@
+function tlk --wraps='tmux list-keys'
+    tmux list-keys $argv
+end
