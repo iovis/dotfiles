cf78480325780e725341eafefe7ac6023387f53b
David Marchante <davidmarchan@gmail.com>
2023-11-19 16:45:36 -0500

    feat(hammerspoon): Change binding of Music

--- hammerspoon/.hammerspoon/application_launcher.lua
+++ hammerspoon/.hammerspoon/application_launcher.lua
@@ -9,10 +9,13 @@ bind(ctrl_alt_cmd, "s", "Slack")
 bind(ctrl_alt_cmd, "d", "WezTerm")
 bind(ctrl_alt_cmd, "f", "Arc")
 bind(ctrl_alt_cmd, "g", "Tower")
+
 bind(ctrl_alt_cmd, "w", "Messages")
 bind(ctrl_alt_cmd, "e", "Mail")
-bind(ctrl_alt_cmd, "b", "Bear")
-bind(ctrl_alt_cmd, "c", "Fantastical")
+
 bind(ctrl_alt_cmd, "z", "zoom.us")
+bind(ctrl_alt_cmd, "x", "Music")
+bind(ctrl_alt_cmd, "c", "Fantastical")
+bind(ctrl_alt_cmd, "b", "Bear")
+
 bind(ctrl_alt_cmd, "n", "Numi")
-bind(hyper, "m", "Music")
