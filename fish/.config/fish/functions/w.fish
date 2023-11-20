3ad123adf0e41ab801c29324df43e4cd74de97f2
David Marchante <davidmarchan@gmail.com>
2023-11-19 20:13:55 -0500

    feat(brew): Replace entr with watchexec

---
+++ fish/.config/fish/functions/w.fish
@@ -0,0 +1,3 @@
+function w --wraps=watchexec --description 'alias w watchexec'
+    watchexec $argv
+end
--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -14,5 +14,5 @@ else
   vim.keymap.set("n", "s<cr>", "<cmd>Tux bear -- make run<cr>", { buffer = true })
   vim.keymap.set("n", "m<cr>", "<cmd>Tux bear -- make<cr>", { buffer = true })
 
-  vim.keymap.set("n", "<leader>sw", "<cmd>Tux ls **/*.c | entr -c make<cr>", { buffer = true })
+  vim.keymap.set("n", "<leader>sw", "<cmd>Tux w -e c -c clear make<cr>", { buffer = true })
 end
