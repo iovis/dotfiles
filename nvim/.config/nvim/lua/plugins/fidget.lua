55e092a36f763ac5c14d94775d38abbcd3b7dab2
David Marchante <davidmarchan@gmail.com>
2023-11-22 16:43:01 -0500

    feat(nvim.fidget): Remove group separator

--- nvim/.config/nvim/lua/plugins/fidget.lua
+++ nvim/.config/nvim/lua/plugins/fidget.lua
@@ -36,6 +36,9 @@ return {
             icon_style = "Title",
           }),
         },
+        view = {
+          group_separator = "",
+        },
         window = {
           winblend = 0,
         },
