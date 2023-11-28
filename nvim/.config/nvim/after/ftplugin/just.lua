7c4a89e349bec03b68f3bc90c5ebb0760d7feeaf
David Marchante <davidmarchan@gmail.com>
2023-11-28 01:17:12 -0500

    feat(nvim.ft.just): Simplify and improve logic to find task under the cursor

--- nvim/.config/nvim/after/ftplugin/just.lua
+++ nvim/.config/nvim/after/ftplugin/just.lua
@@ -8,28 +8,24 @@ vim.keymap.set("n", "s<cr>", function()
   -- Examples:
   --
   -- init:
-  -- build: init
+  -- @build: init
   -- run file="default": build
-  local command_pattern = [[^\(\w\+\).*:\( \|$\)]]
 
-  local command = vim.fn.matchlist( -- Get matches of a pattern in a given text
-    vim.fn.getline( -- Returns the contents of a line given its number
-      vim.fn.search( -- Returns line number of match or 0 if it doesn't exist
-        command_pattern,
-        "bcnW"
-        -- 'b' -> search backward
-        -- 'c' -> accept a match at the cursor position
-        -- 'n' -> don't move the cursor
-        -- 'W' -> don't wrap around the file
-      )
-    ),
-    command_pattern
-  )
+  -- stylua: ignore
+  local command = vim.fn.getline( -- Returns the contents of a line given its number
+    vim.fn.search( -- Returns line number of match or 0 if it doesn't exist
+      [[\v^[@]?\w+.*:( |$)]],
+      "bcnW"
+      -- 'b' -> search backward
+      -- 'c' -> accept a match at the cursor position
+      -- 'n' -> don't move the cursor
+      -- 'W' -> don't wrap around the file
+    )
+  ):match("^@?([%w_]+)")
 
-  if vim.tbl_isempty(command) then
+  if command then
+    vim.cmd.Tux(("just %s"):format(command))
+  else
     vim.notify("No recipe", vim.log.levels.WARN)
-    return
   end
-
-  vim.cmd.Tux(("just %s"):format(command[2]))
 end, { buffer = true, desc = "Run current recipe" })
