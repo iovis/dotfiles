145bbfa32996351f61f4827e8d1d7a06e2791899
David Marchante <davidmarchan@gmail.com>
2023-12-11 10:03:37 -0500

    feat(rust.clippy): Don't warn about range loops

--- fish/.config/fish/functions/clippy.fish
+++ fish/.config/fish/functions/clippy.fish
@@ -1,3 +1,8 @@
 function clippy
-    cargo clippy -- -W clippy::pedantic -A clippy::missing-errors-doc -A clippy::missing-panics-doc -A clippy::must-use-candidate $argv
+    cargo clippy -- -W clippy::pedantic \
+                    -A clippy::missing-errors-doc \
+                    -A clippy::missing-panics-doc \
+                    -A clippy::must-use-candidate \
+                    -A clippy::needless_range_loop \
+                    $argv
 end
