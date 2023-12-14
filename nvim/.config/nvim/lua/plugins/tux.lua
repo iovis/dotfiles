3e4d33d7580f7221b2a451bd4ae7ca1005552098
David Marchante <davidmarchan@gmail.com>
2023-12-14 00:55:31 -0500

    feat(nvim.tux): Update config

--- nvim/.config/nvim/lua/plugins/tux.lua
+++ nvim/.config/nvim/lua/plugins/tux.lua
@@ -1,16 +1,16 @@
 return {
   "iovis/tux.nvim",
-  dev = true,
+  -- dev = true,
   event = "VeryLazy",
   keys = {
     { "c<space>", ":Tux<space>" },
     { "s<space>", ":Tuxw<space>" },
+    { "y<space>", ":Tuxpop<space>" },
 
     -- Repeat command in last tmux split
     { "<leader>i", ":Tux Up<cr>", silent = true },
 
     -- Just
-    { "S", "<cmd>Tux jf<cr>", desc = "Run a task from the Justfile" },
     { "s<cr>", "<cmd>Tux just run<cr>", desc = "just run" },
     { "m<cr>", "<cmd>Tux just build<cr>", desc = "just build" },
 
@@ -20,6 +20,22 @@ return {
   },
   config = function()
     local tux = require("tux")
-    tux.setup({})
+
+    tux.setup({
+      popup = {
+        auto_close = "success",
+        width = "80%",
+        height = "80%",
+      },
+    })
+
+    local u = require("config.utils")
+    vim.keymap.set("n", "S", function()
+      if u.has_justfile() then
+        tux.run("jf")
+      else
+        vim.notify("No justfile found", vim.log.levels.WARN)
+      end
+    end, { desc = "Run a task from the Justfile" })
   end,
 }
