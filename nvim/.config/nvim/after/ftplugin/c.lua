2d1350f462a4bcd5734ddf5e0cf69343c8c0b414
David Marchante <davidmarchan@gmail.com>
2023-12-26 11:56:23 -0500

    feat(nvim.ft.c): Change binding for compiling ruby extensions

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -5,8 +5,8 @@ vim.keymap.set("n", "<leader>aa", "<cmd>ClangdSwitchSourceHeader<cr>", { buffer 
 ---- runnables
 if vim.fn.expand("%"):match("ext/") then
   ---- Ruby C Extension
-  vim.keymap.set("n", "m<cr>", "<cmd>Tux bear -- rake compile<cr>", { buffer = true })
-  vim.keymap.set("n", "s<cr>", "<cmd>Tux bear -- rake compile && rspec<cr>", { buffer = true })
+  vim.keymap.set("n", "m<cr>", "<cmd>Tux bear -- rake<cr>", { buffer = true })
+  vim.keymap.set("n", "s<cr>", "<cmd>Tux bear -- rake<cr>", { buffer = true })
 elseif u.has_justfile() then
   vim.keymap.set("n", "s<cr>", "<cmd>Tux bear -- just run<cr>", { buffer = true })
   vim.keymap.set("n", "m<cr>", "<cmd>Tux bear -- just build<cr>", { buffer = true })
