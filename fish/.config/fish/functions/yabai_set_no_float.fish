5179b9be81ec78a8f018149852506cbb293241d5
David Marchante <davidmarchan@gmail.com>
2024-01-05 15:01:15 -0500

    feat(yabai): Make `yabai_set_no_float` also reset zoom

--- fish/.config/fish/functions/yabai_set_no_float.fish
+++ fish/.config/fish/functions/yabai_set_no_float.fish
@@ -1,5 +1,18 @@
 function yabai_set_no_float
-    if test (yabai -m query --windows --window | jq '."is-floating"') = true
+    set -l window_settings (yabai -m query --windows --window)
+
+    if test (echo $window_settings | jq '."is-floating"') = true
         yabai -m window --toggle float
+        return
+    end
+
+    if test (echo $window_settings | jq '."has-fullscreen-zoom"') = true
+        yabai -m window --toggle zoom-fullscreen
+        return
+    end
+
+    if test (echo $window_settings | jq '."has-parent-zoom"') = true
+        yabai -m window --toggle zoom-parent
+        return
     end
 end
