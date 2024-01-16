7864f758695beda0e7d6a29cf4ba307b339138d9
David Marchante <davidmarchan@gmail.com>
2024-01-15 22:58:39 -0500

    feat(nvim.snippets.c): Add snippet for Ruby handler

--- nvim/.config/nvim/lua/snippets/c.lua
+++ nvim/.config/nvim/lua/snippets/c.lua
@@ -1,22 +1,11 @@
 local ls = require("luasnip")
 local s = ls.snippet
--- local sn = ls.snippet_node
--- local t = ls.text_node
 local i = ls.insert_node
--- local f = ls.function_node
 local c = ls.choice_node
--- local d = ls.dynamic_node
 local r = ls.restore_node
--- local l = require("luasnip.extras").lambda
--- local rep = require("luasnip.extras").rep
--- local p = require("luasnip.extras").partial
--- local m = require("luasnip.extras").match
--- local n = require("luasnip.extras").nonempty
--- local dl = require("luasnip.extras").dynamic_lambda
--- local fmt = require("luasnip.extras.fmt").fmt
+local n = require("luasnip.extras").nonempty
 local fmta = require("luasnip.extras.fmt").fmta
 local conds = require("luasnip.extras.conditions")
--- local parse = ls.parser.parse_snippet
 
 return {
   s(
@@ -64,6 +53,23 @@ return {
         ret_type = i(1, "void"),
         fname = i(2, "fname"),
         args = i(3, "void"),
+        body = i(0, "// body"),
+      }
+    ),
+    { condition = conds.line_begin }
+  ),
+  s(
+    "rf",
+    fmta(
+      [[
+        static VALUE <fname>(VALUE self<comma><args>) {
+          <body>
+        }
+      ]],
+      {
+        fname = i(1, "fname"),
+        comma = n(2, ", "),
+        args = i(2),
         body = i(0, "// body"),
       }
     ),
