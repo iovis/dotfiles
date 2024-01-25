98e731b34de9dae20c4dca88b6c232d6e9809e31
David Marchante <davidmarchan@gmail.com>
2024-01-25 17:33:56 -0500

    chore(nvim.plugins.muxi): Save `muxi` in a variable

--- nvim/.config/nvim/lua/plugins/muxi.lua
+++ nvim/.config/nvim/lua/plugins/muxi.lua
@@ -5,7 +5,9 @@ return {
   event = "VeryLazy",
   config = function()
     ----Setup
-    require("muxi").setup({
+    local muxi = require("muxi")
+
+    muxi.setup({
       go_to_cursor = false,
       -- path = ".muxi.json",
     })
@@ -31,18 +33,18 @@ return {
 
     for _, key in ipairs(keys) do
       vim.keymap.set("n", "g" .. key:upper(), function()
-        require("muxi").add(key)
+        muxi.add(key)
         vim.notify("Added current file to " .. key)
       end, { desc = "[muxi] Add session to " .. key })
 
       vim.keymap.set("n", "g" .. key, function()
-        require("muxi").go_to(key, { go_to_cursor = false })
+        muxi.go_to(key, { go_to_cursor = false })
       end, { desc = "[muxi] go to session " .. key })
     end
 
     -- ñ:upper() doesn't work great
     vim.keymap.set("n", "gÑ", function()
-      require("muxi").add("ñ")
+      muxi.add("ñ")
       vim.notify("Added current file to ñ")
     end, { desc = "[muxi] Add session to ñ" })
 
@@ -56,7 +58,7 @@ return {
     })
 
     vim.keymap.set("n", "m!", function()
-      require("muxi").clear_all()
+      muxi.clear_all()
       vim.notify("Cleared current session")
     end, { desc = "[muxi] Clear current workspace" })
 
@@ -66,8 +68,6 @@ return {
 
     ----Settings toggles
     vim.keymap.set("n", "yom", function()
-      local muxi = require("muxi")
-
       muxi.config.go_to_cursor = not muxi.config.go_to_cursor
 
       local message = "Muxi go to cursor "
@@ -80,7 +80,7 @@ return {
     -- vim.keymap.set("n", "<leader>gd", require("muxi.ui").delete_prompt, { desc = "[muxi] Interactive delete" })
     --
     -- vim.keymap.set("n", "<leader>gM", function()
-    --   local muxi_path = require("muxi").config.path
+    --   local muxi_path = muxi.config.path
     --   vim.cmd.split(muxi_path)
     --   vim.keymap.set("n", "q", "<cmd>q!<cr>", { buffer = true })
     -- end, { desc = "[muxi] Open storage" })
