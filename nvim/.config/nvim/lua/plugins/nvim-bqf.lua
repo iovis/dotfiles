0ed67731bdbbe097792fbb162fb8b7f25e6fba24
David Marchante <davidmarchan@gmail.com>
2024-01-27 00:06:43 -0500

    feat(nvim.plugins.bqf): Tweak nvim-bqf

--- nvim/.config/nvim/lua/plugins/nvim-bqf.lua
+++ nvim/.config/nvim/lua/plugins/nvim-bqf.lua
@@ -1,27 +1,13 @@
 return {
   "kevinhwang91/nvim-bqf",
-  event = "VeryLazy",
+  ft = "qf",
   dependencies = {
     "junegunn/fzf",
     version = "*",
   },
   config = function()
     require("bqf").setup({
-      filter = {
-        fzf = {
-          extra_opts = {
-            "--bind",
-            ""
-              .. "ctrl-p:page-up,"
-              .. "ctrl-n:page-down,"
-              .. "alt-a:select-all,"
-              .. "alt-d:deselect-all,"
-              .. "alt-t:toggle-all",
-          },
-        },
-      },
       func_map = {
-        ptoggleauto = "p",
         split = "<c-s>",
       },
       preview = {
