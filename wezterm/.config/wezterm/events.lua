19628a9632ea1bbfba87e02c08c57047df514a9a
David Marchante <davidmarchan@gmail.com>
2024-01-29 14:08:23 -0500

    feat(wezterm): new release

--- wezterm/.config/wezterm/events.lua
+++ wezterm/.config/wezterm/events.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 
-wezterm.on("toggle-opacity", function(window, pane)
+wezterm.on("toggle-opacity", function(window, _pane)
   local overrides = window:get_config_overrides() or {}
 
   if not overrides.window_background_opacity then
