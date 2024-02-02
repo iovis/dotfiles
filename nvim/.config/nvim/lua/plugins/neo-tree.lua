3a8a7e9556a4e6ef27a157c131e6e257689d4ac3
David Marchante <davidmarchan@gmail.com>
2024-02-01 23:44:01 -0500

    feat(nvim.plugins.neotree): Tweak fold_{left,right} further

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -78,22 +78,20 @@ return {
           fold_left = function(state)
             local node = state.tree:get_node()
             local parent_id = node:get_parent_id()
-
-            require("neo-tree.ui.renderer").focus_node(state, parent_id)
-
             local parent = state.tree:get_node(parent_id)
-            if parent.type == "directory" and parent:is_expanded() then
+
+            if node.type == "directory" and node:is_expanded() then
+              require("neo-tree.sources.filesystem").toggle_directory(state, node)
+            else
+              require("neo-tree.ui.renderer").focus_node(state, parent_id)
               require("neo-tree.sources.filesystem").toggle_directory(state, parent)
             end
           end,
 
           fold_right = function(state)
             local node = state.tree:get_node()
-            if node.type ~= "directory" then
-              return
-            end
 
-            if not node:is_expanded() then
+            if node.type == "directory" and not node:is_expanded() then
               require("neo-tree.sources.filesystem").toggle_directory(state, node)
             end
 
