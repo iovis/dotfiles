c8b8e9a97661475c833dde2fba5e79babd4633fe
David Marchante <davidmarchan@gmail.com>
2024-03-04 11:18:32 -0500

    fix(nvim.plugins.oil): Fix deprecation on `lsp_rename_autosave`

---
+++ nvim/.config/nvim/after/ftplugin/oil_preview.lua
@@ -0,0 +1,6 @@
+vim.keymap.set("n", "<cr>", "o", {
+  desc = "Accept changes with <cr>",
+  buffer = true,
+  remap = true,
+  nowait = true,
+})
--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -15,14 +15,10 @@ return {
 
     oil.setup({
       default_file_explorer = true,
-      lsp_rename_autosave = true,
       skip_confirm_for_simple_edits = true,
       float = {
         max_height = 20,
         max_width = 75,
-        win_options = {
-          winblend = 0,
-        },
       },
       keymaps = {
         P = "actions.preview",
@@ -40,6 +36,9 @@ return {
         ["~"] = false,
         ["g\\"] = false,
       },
+      lsp_file_methods = {
+        autosave_changes = true,
+      },
       view_options = {
         show_hidden = true,
       },
@@ -49,25 +48,5 @@ return {
         signcolumn = "yes",
       },
     })
-
-    local oil_augroup = vim.api.nvim_create_augroup("oil_augroup", { clear = true })
-    vim.api.nvim_create_autocmd("FileType", {
-      desc = "Accept changes with <CR>",
-      group = oil_augroup,
-      pattern = "oil_preview",
-      callback = function(params)
-        vim.keymap.set("n", "<cr>", "o", {
-          buffer = params.buf,
-          remap = true,
-          nowait = true,
-        })
-      end,
-    })
-
-    -- Fugitive expects netrw to exist, otherwise to define your own `:Browse`
-    vim.api.nvim_create_user_command("Browse", function(opts)
-      -- bowser's castle
-      vim.cmd.Browser(vim.fn.escape(opts.args, "%#!"))
-    end, { nargs = 1 })
   end,
 }
