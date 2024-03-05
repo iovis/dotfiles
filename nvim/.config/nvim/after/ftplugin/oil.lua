583871ef4bcc702fd415aef192fab8016728363a
David Marchante <davidmarchan@gmail.com>
2024-03-04 21:49:54 -0500

    feat(nvim.plugins.oil): Not sure how I feel about the winbar yet

--- nvim/.config/nvim/after/ftplugin/oil.lua
+++ nvim/.config/nvim/after/ftplugin/oil.lua
@@ -1,3 +1,3 @@
 -- oil.get_current_dir()
 -- NOTE: Disable when using `--float`
-vim.wo.winbar = "%#barbecue_separator#        %{%v:lua.require'oil'.get_current_dir()%}"
+-- vim.wo.winbar = "%#barbecue_separator#        %{%v:lua.require'oil'.get_current_dir()%}"
