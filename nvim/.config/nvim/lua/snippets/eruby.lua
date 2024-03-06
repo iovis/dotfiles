feefc55ebcc3d4b094fc8a688d841c8e317a343d
David Marchante <davidmarchan@gmail.com>
2024-03-05 23:46:49 -0500

    feat(nvim.snippets.erb): Add some erb snippets

--- nvim/.config/nvim/lua/snippets/eruby.lua
+++ nvim/.config/nvim/lua/snippets/eruby.lua
@@ -1,3 +1,69 @@
 return {
-  parse("=", "<%= $1 %>"),
+  s("=", fmt("<%= {} %>", { i(1) }), { condition = conds.line_begin }),
+  s("%", fmt("<% {} %>", { i(1) }), { condition = conds.line_begin }),
+  s(
+    "link_to",
+    fmt("<%= link_to {}, {} %>", {
+      i(1, "'Text'"),
+      i(2, "root_path"),
+    }),
+    { condition = conds.line_begin }
+  ),
+  s(
+    "render",
+    fmt("<%= render {} %>", {
+      i(1, "partial"),
+    }),
+    { condition = conds.line_begin }
+  ),
+  s(
+    "each",
+    fmt(
+      [[
+        <% {}.each do |{}| %>
+          {}
+        <% end %>
+      ]],
+      {
+        i(1, "list"),
+        i(2, "item"),
+        i(0),
+      }
+    ),
+    { condition = conds.line_begin }
+  ),
+  s(
+    "if",
+    fmt(
+      [[
+        <% if {} %>
+          {}
+        <% end %>
+      ]],
+      {
+        i(1),
+        i(2),
+      }
+    ),
+    { condition = conds.line_begin }
+  ),
+  s(
+    "if",
+    fmt(
+      [[
+        <% if {} %>
+          {}
+        <% else %>
+          {}
+        <% end %>
+      ]],
+      {
+        i(1),
+        i(2),
+        i(3),
+      }
+    ),
+    { condition = conds.line_begin }
+  ),
+  s("pry", t("<% require 'pry'; binding.pry %>"), { condition = conds.line_begin }),
 }
