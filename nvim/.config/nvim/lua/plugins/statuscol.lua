7e47f53887e08ca87b58de1e3f38eb17b2b87c43
David Marchante <davidmarchan@gmail.com>
2024-03-08 13:12:00 -0500

    fix(nvim.plugins.statuscol): Add padding for fold symbols

--- nvim/.config/nvim/lua/plugins/statuscol.lua
+++ nvim/.config/nvim/lua/plugins/statuscol.lua
@@ -8,7 +8,7 @@ return {
       relculright = true, -- right align current line
       segments = {
         { text = { "%s" }, click = "v:lua.ScSa" },
-        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+        { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
         { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
       },
     })
