ab6c26d8fa8bd2258cde782669f249fcce754c3a
David Marchante <davidmarchan@gmail.com>
2024-03-26 15:30:08 -0400

    feat(fish): Update podman_reset function

--- fish/.config/fish/functions/podman_reset.fish
+++ fish/.config/fish/functions/podman_reset.fish
@@ -1,5 +1,4 @@
-function podman_reset --wraps='podman machine stop && podman machine rm podman-machine-default && podman machine init --cpus 2 --memory 2048 --disk-size 40 && podman machine start' --description 'alias podman_reset podman machine stop && podman machine rm podman-machine-default && podman machine init --cpus 2 --memory 2048 --disk-size 40 && podman machine start'
-    podman machine stop
-    podman machine rm -f podman-machine-default
-    podman machine init --cpus 2 --memory 2048 --disk-size 40 && podman machine start $argv
+function podman_reset
+    podman machine reset -f
+    podman machine init --cpus 2 --memory 2048 --disk-size 40 && podman machine start
 end
