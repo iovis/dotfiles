df9ab383ca7e69b0f349a28c393aa07dd3e50f24
David Marchante <davidmarchan@gmail.com>
2024-04-14 12:42:52 -0400

    feat(nvim.plugins.rest): upgrade rest-nvim

--- nvim/.config/nvim/after/ftplugin/http.lua
+++ nvim/.config/nvim/after/ftplugin/http.lua
@@ -1,4 +1,1 @@
-local rest = require("rest-nvim")
-
-vim.keymap.set("n", "s<cr>", rest.run, { buffer = true })
-vim.keymap.set("n", "m<cr>", "<Plug>RestNvimPreview", { buffer = true })
+vim.keymap.set("n", "s<cr>", "<cmd>Rest run<cr>", { buffer = true })
--- nvim/.config/nvim/lua/plugins/rest_nvim.lua
+++ nvim/.config/nvim/lua/plugins/rest_nvim.lua
@@ -1,17 +1,23 @@
 return {
-  "rest-nvim/rest.nvim",
-  enabled = false,
-  event = "VeryLazy",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("rest-nvim").setup({
-      result_split_in_place = true,
-      result = {
-        show_curl_command = false,
-        show_http_info = true,
-        show_headers = true,
-      },
-      jump_to_request = false,
-    })
-  end,
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+  },
+  {
+    "rest-nvim/rest.nvim",
+    ft = "http",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      require("rest-nvim").setup({
+        highlight = { timeout = 100 },
+        result = {
+          keybinds = {
+            prev = "<",
+            next = ">",
+          },
+        },
+      })
+    end,
+  },
 }
