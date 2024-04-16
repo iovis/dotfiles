779ff175ec25198c3b3da4dbb8618649d735bd12
David Marchante <davidmarchan@gmail.com>
2024-04-15 20:38:30 -0400

    fix(nvim.plugins.toggleterm): don't show winbar in toggleterm
    Apparently there's an issue if you have start in insert mode on

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -4,6 +4,11 @@ return {
   config = function()
     require("toggleterm").setup({
       open_mapping = [[<c-\>]],
+      on_open = function(term)
+        vim.defer_fn(function()
+          vim.wo[term.window].winbar = ""
+        end, 0)
+      end,
     })
   end,
 }
