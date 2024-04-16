77f057fcf9048403ccc897895eb735621dc08906
David Marchante <davidmarchan@gmail.com>
2024-04-16 10:34:35 -0400

    feat(nvim.plugins.tmux-navigator): allow to navigate also in terminal panes

--- nvim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -2,10 +2,10 @@ return {
   "christoomey/vim-tmux-navigator",
   event = "VeryLazy",
   keys = {
-    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>", mode = { "n", "t" } },
+    { "<c-j>", "<cmd>TmuxNavigateDown<cr>", mode = { "n", "t" } },
+    { "<c-k>", "<cmd>TmuxNavigateUp<cr>", mode = { "n", "t" } },
+    { "<c-l>", "<cmd>TmuxNavigateRight<cr>", mode = { "n", "t" } },
   },
   init = function()
     vim.g.tmux_navigator_no_mappings = 1
