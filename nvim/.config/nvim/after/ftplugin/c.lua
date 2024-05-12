228e1f041aad2b7d320eb6e41a8a620d9818cfaf
David Marchante <davidmarchan@gmail.com>
2024-05-12 14:33:43 -0400

    feat: use a popup for qmk stuff

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -4,16 +4,18 @@ vim.bo.commentstring = "// %s"
 
 vim.keymap.set("n", "<leader>al", "<cmd>ClangdSwitchSourceHeader<cr>", { buffer = true })
 
-if vim.fn.expand("%"):match("keyboards/") then
-  vim.keymap.set("n", "d<cr>", "<cmd>botright split! ../qmk_firmware/docs/keycodes.md<cr>", { buffer = true })
-end
-
 ---- runnables
 if vim.fn.expand("%"):match("ext/") then
   ---- Ruby C Extension
   vim.keymap.set("n", "m<cr>", "<cmd>Tux bear -- rake<cr>", { buffer = true })
   vim.keymap.set("n", "s<cr>", "<cmd>Tux bear -- rake<cr>", { buffer = true })
   vim.keymap.set("n", "<leader>sw", "<cmd>Tux watchexec -e c,h,rb -c clear -- bear -- rake<cr>", { buffer = true })
+elseif vim.fn.expand("%"):match("keyboards/") then
+  ---- QMK
+  vim.keymap.set("n", "s<cr>", "<cmd>Tuxpopup just compile<cr>", { buffer = true })
+  vim.keymap.set("n", "m<cr>", "<cmd>Tuxpopup just flash<cr>", { buffer = true })
+
+  vim.keymap.set("n", "d<cr>", "<cmd>botright split! ../qmk_firmware/docs/keycodes.md<cr>", { buffer = true })
 elseif u.has_justfile() then
   vim.keymap.set("n", "s<cr>", "<cmd>Tux just run<cr>", { buffer = true })
   vim.keymap.set("n", "m<cr>", "<cmd>Tux just build<cr>", { buffer = true })
