0005f5554a19eff678283cfe6e92ee3c845e05c2
David Marchante <davidmarchan@gmail.com>
2024-05-21 15:44:14 -0400

    feat(zsh): bindings for autosuggestions

--- zsh/.config/zsh/bindkey.zsh
+++ zsh/.config/zsh/bindkey.zsh
@@ -7,5 +7,6 @@ bindkey "^[[A" up-line-or-beginning-search # Up
 autoload -U down-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey "^[[B" down-line-or-beginning-search # Down
-# bindkey "^N" autosuggest-accept
-# bindkey "^Y" autosuggest-accept
+
+bindkey "^N" forward-char
+bindkey "^P" forward-word
