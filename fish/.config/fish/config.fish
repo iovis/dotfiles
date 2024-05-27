bad73dd8c63df042a4e4edcbc2284c6949bc2b12
David Marchante <davidmarchan@gmail.com>
2024-05-27 13:17:32 -0400

    fix(xdg): fix xdg paths
    You'd think it would take a special kind of dumbass to get this wrong,
    but I am a special kind of dumbass

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,8 @@
 ## Environment
+set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_CONFIG_HOME $HOME/.config
-set -gx XDG_DATA_HOME $XDG_CONFIG_HOME/.local/share
-set -gx XDG_CACHE_HOME $XDG_CONFIG_HOME/.cache
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_STATE_HOME $HOME/.local/state
 
 set -gx FDOTDIR $XDG_CONFIG_HOME/fish
 set -gx ICLOUD_PATH "$HOME/Library/Mobile Documents/com~apple~CloudDocs"
