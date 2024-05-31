fd60a58132632e8ef646a3b4dbfa8e038c227896
David Marchante <davidmarchan@gmail.com>
2024-05-31 09:49:56 -0400

    feat(zsh.aliases): add `jf`

--- zsh/.config/zsh/command_config.zsh
+++ zsh/.config/zsh/command_config.zsh
@@ -23,6 +23,7 @@ fi
 # just
 if type just > /dev/null; then
   alias j="just"
+  alias jf="just --choose --chooser \"fzf-tmux -p50\%,50\% --prompt 'just> ' --reverse --info inline --preview 'just --show {}' --preview-window 'down'\""
 fi
 
 # lazygit
