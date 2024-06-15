9e8642bdb74798cfb454c23c3da6a7d2901de879
David Marchante <davidmarchan@gmail.com>
2024-06-15 14:34:27 -0400

    chore(nvim): cleanup

--- nvim/.config/nvim/lua/plugins/blame.lua
+++ nvim/.config/nvim/lua/plugins/blame.lua
@@ -11,25 +11,5 @@ return {
       date_format = "%Y-%m-%d",
       commit_detail_view = "tab",
     })
-
-    -- vim.api.nvim_create_autocmd("User", {
-    --   pattern = "BlameViewOpened",
-    --   callback = function(event)
-    --     local blame_type = event.data
-    --     if blame_type == "window" then
-    --       require("barbecue.ui").toggle(false)
-    --     end
-    --   end,
-    -- })
-    --
-    -- vim.api.nvim_create_autocmd("User", {
-    --   pattern = "BlameViewClosed",
-    --   callback = function(event)
-    --     local blame_type = event.data
-    --     if blame_type == "window" then
-    --       require("barbecue.ui").toggle(true)
-    --     end
-    --   end,
-    -- })
   end,
 }
