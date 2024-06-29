29d465da48627ad5de3fedf3064217945ce58e5b
David Marchante <davidmarchan@gmail.com>
2024-06-28 22:38:09 -0400

    chore(fish): clean up vim_plugins

--- fish/.config/fish/functions/vim_plugins.fish
+++
@@ -1,3 +0,0 @@
-function vim_plugins --wraps='cd /Users/david/.config/.local/share/nvim/lazy/' --description 'alias vim_plugins=cd /Users/david/.config/.local/share/nvim/lazy/'
-    cd /Users/david/.config/.local/share/nvim/lazy/ $argv
-end
