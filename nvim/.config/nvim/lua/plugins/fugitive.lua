8d68bec75a57c4b010f99e0c003ca1fb612e8c2a
David Marchante <davidmarchan@gmail.com>
2024-07-07 18:40:23 -0400

    feat(nvim.plugins.fugitive): clean up with lua

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -11,24 +11,22 @@ return {
     vim.keymap.set("n", "<leader>G", "<cmd>Gtabedit:<cr>)", { remap = true })
     vim.keymap.set("n", "<leader>go", "<cmd>Gread<cr>")
     -- vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>")
-    vim.keymap.set("n", "<leader>lg", "<cmd>Glol -500<cr>")
     vim.keymap.set({ "n", "x" }, "<leader>gg", ":GBrowse<cr>", { silent = true })
 
-    -- NOTE: Fugitive expects netrw to exist, otherwise to define your own `:Browse`
-    --       Right now I'm using `oil.nvim` as a netrw replacement
-    vim.api.nvim_create_user_command("Browse", function(opts)
+    vim.keymap.set("n", "<leader>lg", "<cmd>Glol -500<cr>")
+    u.command("Glol", [[Git log --graph --pretty='%h -%d %s (%cr) <%an>' <args>]], {
+      nargs = "*",
+    })
+
+    vim.keymap.set("x", "<leader>lg", ":GLogL<cr>", { silent = true })
+    u.command("GLogL", [[Git log -L <line1>,<line2>:% <args>]], {
+      range = true,
+      nargs = "*",
+    })
+
+    -- NOTE: Fugitive expects netrw to exist, or to define your own `:Browse`
+    u.command("Browse", function(opts)
       vim.ui.open(opts.args)
-    end, { nargs = 1, force = true })
-
-    vim.cmd([[
-      command! -nargs=*        Glol Git log --graph --pretty='%h -%d %s (%cr) <%an>' <args>
-      command! -range -nargs=* GLogL Git log -L <line1>,<line2>:% <args>
-
-      command! -nargs=0 Gcm   !git checkout main
-      command! -nargs=0 Gcq   !git checkout qa
-      command! -nargs=0 Gpsup !git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)
-      command! -nargs=0 Grhh  !git reset --hard
-      command! -nargs=1 Gcb   !git checkout -b <args>
-    ]])
+    end, { force = true, nargs = 1 })
   end,
 }
