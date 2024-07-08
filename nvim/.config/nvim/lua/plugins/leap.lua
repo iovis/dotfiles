755af2a8705ee0b5a5be59673f90360a5e3f3366
David Marchante <davidmarchan@gmail.com>
2024-07-08 09:12:58 -0400

    feat(nvim.plugins.leap): go back to leap.nvim

---
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -0,0 +1,14 @@
+return {
+  "ggandor/leap.nvim",
+  event = "VeryLazy",
+  config = function()
+    local leap = require("leap")
+
+    leap.setup({})
+
+    vim.keymap.set({ "n", "x", "o" }, "f", "<Plug>(leap-forward-to)")
+    vim.keymap.set({ "n", "x", "o" }, "F", "<Plug>(leap-backward-to)")
+
+    leap.add_repeat_mappings(";", ",")
+  end,
+}
