532f10110669514ceecab84062a2893a98207ce7
David Marchante <davidmarchan@gmail.com>
2024-07-08 18:07:46 -0400

    feat(nvim.plugins.leap): add "till" mappings

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -8,6 +8,8 @@ return {
 
     vim.keymap.set({ "n", "x", "o" }, "f", "<Plug>(leap-forward-to)")
     vim.keymap.set({ "n", "x", "o" }, "F", "<Plug>(leap-backward-to)")
+    vim.keymap.set({ "x", "o" }, "t", "<Plug>(leap-forward-till)")
+    vim.keymap.set({ "x", "o" }, "T", "<Plug>(leap-backward-till)")
 
     leap.add_repeat_mappings(";", ",")
   end,
