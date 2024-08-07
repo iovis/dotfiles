ea61989f2cf3ae3a55833189b2d35a17cb14b983
David Marchante <davidmarchan@gmail.com>
2024-08-06 21:08:32 -0400

    feat(nvim.ft.zig): use relative path for runners

--- nvim/.config/nvim/after/ftplugin/zig.lua
+++ nvim/.config/nvim/after/ftplugin/zig.lua
@@ -6,6 +6,6 @@ if u.current_file():match("src/") and not u.has_justfile() then
   vim.keymap.set("n", "s<cr>", "<cmd>Tux zig build run<cr>", { buffer = true })
   vim.keymap.set("n", "m<cr>", "<cmd>Tux zig build<cr>", { buffer = true })
 else
-  vim.keymap.set("n", "s<cr>", "<cmd>Tux zig run %<cr>", { buffer = true })
-  vim.keymap.set("n", "m<cr>", "<cmd>Tux zig build-exe %<cr>", { buffer = true })
+  vim.keymap.set("n", "s<cr>", "<cmd>Tux zig run %:.<cr>", { buffer = true })
+  vim.keymap.set("n", "m<cr>", "<cmd>Tux zig build-exe %:.<cr>", { buffer = true })
 end
