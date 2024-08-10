e68908434e76cfbca1b44dc6a83106981a5ba656
David Marchante <davidmarchan@gmail.com>
2024-08-10 00:15:37 -0400

    chore(nvim.plugins.substitute.vim): RIP substitute.vim
    You will be remembered

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -3,7 +3,6 @@ vim.cmd.packadd("cfilter")
 return {
   { "chrisbra/csv.vim", ft = "csv" },
   { "dstein64/vim-startuptime", cmd = "StartupTime" },
-  { "iovis/substitute.vim", event = "VeryLazy" },
   { "tpope/vim-dadbod", cmd = { "DB" }, keys = { { "d<space>", ":DB<space>" } } },
   { "tpope/vim-eunuch", event = "VeryLazy" }, -- UNIX commands
   { "vim-test/vim-test", event = "VeryLazy" },
