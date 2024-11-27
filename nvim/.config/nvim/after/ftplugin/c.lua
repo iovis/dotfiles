9a1b6b39b037fb6cb86334009b752ad39efd4bd2
David Marchante <davidmarchan@gmail.com>
2024-11-27 11:32:29 -0500

    feat(qmk): refactor qmk recipes

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -1,3 +1,4 @@
+local tux = require("tux")
 local u = require("config.utils")
 
 vim.bo.commentstring = "// %s"
@@ -10,24 +11,29 @@ if u.current_file():match("ext/") then
   vim.keymap.set("n", "m<cr>", "<cmd>Tux bear -- rake<cr>", { buffer = true })
   vim.keymap.set("n", "s<cr>", "<cmd>Tux bear -- rake<cr>", { buffer = true })
   vim.keymap.set("n", "<leader>sw", "<cmd>Tux watchexec -e c,h,rb -c clear -- bear -- rake<cr>", { buffer = true })
-elseif u.current_file():match("unicorne/") then
-  ---- QMK
-  vim.keymap.set("n", "<leader>S", "<cmd>Tuxpopup just setup<cr>", { buffer = true })
+elseif vim.uv.cwd():match("qmk_userspace") then
+  vim.keymap.set("n", "<leader>S", function()
+    local firmware = "qmk"
+
+    if u.current_file():match("zsa/") then
+      firmware = "zsa"
+    end
+
+    tux.popup("just setup " .. firmware)
+  end, { buffer = true })
+
   vim.keymap.set("n", "s<cr>", "<cmd>Tuxpopup just compile flash<cr>", { buffer = true })
   vim.keymap.set("n", "m<cr>", "<cmd>Tuxpopup just flash<cr>", { buffer = true })
 
   vim.keymap.set("n", "<leader>do", "<cmd>botright split! ../qmk_firmware/docs/keycodes.md<cr>", { buffer = true })
-  vim.keymap.set("n", "d<cr>", "<cmd>silent !tmux new-window -Sn qmk -c ../qmk_firmware<cr>", { buffer = true })
   vim.keymap.set("n", "S", "<cmd>15sp keymap.md<cr>", { buffer = true })
-elseif u.current_file():match("voyager/") then
-  ---- ZSA
-  vim.keymap.set("n", "<leader>S", "<cmd>Tuxpopup just vsetup<cr>", { buffer = true })
-  vim.keymap.set("n", "s<cr>", "<cmd>Tuxpopup just vcompile vflash<cr>", { buffer = true })
-  vim.keymap.set("n", "m<cr>", "<cmd>Tuxpopup just vflash<cr>", { buffer = true })
 
-  vim.keymap.set("n", "<leader>do", "<cmd>botright split! ../qmk_firmware/docs/keycodes.md<cr>", { buffer = true })
-  vim.keymap.set("n", "d<cr>", "<cmd>silent !tmux new-window -Sn zsa -c ../zsa_firmware<cr>", { buffer = true })
-  vim.keymap.set("n", "S", "<cmd>15sp keymap.md<cr>", { buffer = true })
+  vim.keymap.set("n", "d<cr>", function()
+    tux.window("qmk cd", {
+      name = "firmware",
+      select = true,
+    })
+  end, { buffer = true })
 elseif u.has_justfile() then
   vim.keymap.set("n", "s<cr>", "<cmd>Tux just run<cr>", { buffer = true })
   vim.keymap.set("n", "m<cr>", "<cmd>Tux just build<cr>", { buffer = true })
