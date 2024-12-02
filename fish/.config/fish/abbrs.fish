9ca9bef4ae27616bc4f384ce03278d3f0cf4acae
David Marchante <davidmarchan@gmail.com>
2024-12-02 11:54:59 -0500

    feat(fish): update abbreviations

--- fish/.config/fish/abbrs.fish
+++ fish/.config/fish/abbrs.fish
@@ -1,20 +1,21 @@
 ## bundler
-abbr be   bundle exec
-abbr bi   bundle install
+abbr be bundle exec
+abbr bi bundle install
 abbr bout bundle outdated
-abbr bu   bundle update
+abbr bu bundle update
 
 ## directories
-abbr -- -  'cd -'
-abbr ...   'cd ../..'
-abbr ....  'cd ../../..'
+abbr -- - 'cd -'
+abbr ... 'cd ../..'
+abbr .... 'cd ../../..'
 
 ## ruby
-abbr RED  RAILS_ENV=development
-abbr REP  RAILS_ENV=production
-abbr REQ  RAILS_ENV=qa
-abbr RET  RAILS_ENV=test
+abbr RED RAILS_ENV=development
+abbr REP RAILS_ENV=production
+abbr REQ RAILS_ENV=qa
+abbr RET RAILS_ENV=test
 
 ## rust
-abbr RB RUST_BACKTRACE=1
-abbr RL RUST_LOG=debug
+abbr rbt RUST_BACKTRACE=1
+abbr rld RUST_LOG=debug
+abbr rli RUST_LOG=info
