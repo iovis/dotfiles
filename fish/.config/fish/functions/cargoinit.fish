35fff7344a9f417a2d9b470ed13a24d9c9fad28e
David Marchante <davidmarchan@gmail.com>
2024-12-01 20:04:36 -0500

    feat(fish): leverage `cargo-binstall` for managing crates

--- fish/.config/fish/functions/cargoinit.fish
+++ fish/.config/fish/functions/cargoinit.fish
@@ -1,7 +1,11 @@
 function cargoinit
-    cargo install $(cat $DOTFILES/default/crates)
+    if not cargo binstall --help &>/dev/null
+        cargo install cargo-binstall
+    end
+
+    cargo binstall -y $(cat $DOTFILES/default/crates)
 
     if test -n "$RUST_EXTRA"
-        cargo install $(cat $DOTFILES/default/crates_extra)
+        cargo install -y $(cat $DOTFILES/default/crates_extra)
     end
 end
---
+++ fish/.config/fish/functions/cin.fish
@@ -0,0 +1,3 @@
+function cin --wraps='cargo binstall -y' --description 'alias cin cargo binstall -y'
+    cargo binstall -y $argv
+end
