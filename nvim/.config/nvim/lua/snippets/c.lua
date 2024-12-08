4652550a50b8fb65033c775011adc57efb8de3a0
David Marchante <davidmarchan@gmail.com>
2024-12-08 04:05:03 -0500

    feat(nvim.snippets.c): add snippets for C unit testing with unity

--- nvim/.config/nvim/lua/snippets/c.lua
+++ nvim/.config/nvim/lua/snippets/c.lua
@@ -110,4 +110,56 @@ return {
     ),
     { condition = conds.line_begin }
   ),
+  s(
+    "munit",
+    fmta(
+      [[
+        #include "unity.h"
+
+        #include "<>"
+
+        test<>
+
+        const MunitTest munit_null_test = {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL};
+        MunitTest test_suite_tests[] = {
+          {"/test_name", test_name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+          munit_null_test,
+        };
+
+        const MunitSuite test_suite = {
+          "suite_name",
+          test_suite_tests,
+          NULL,
+          1,
+          MUNIT_SUITE_OPTION_NONE
+        };
+
+        int main(int argc, char *argv[]) {
+          return munit_suite_main(&test_suite, NULL, argc, argv);
+        }
+      ]],
+      {
+        i(1, "my_library.h"),
+        i(0),
+      }
+    ),
+    { condition = conds.line_begin }
+  ),
+  s(
+    "test",
+    fmta(
+      [[
+        static MunitResult test_<test_name>(const MunitParameter params[], void *data) {
+          <>
+
+          return MUNIT_OK;
+        }
+      ]],
+      {
+        test_name = i(1, "name"),
+        i(0),
+      }
+    ),
+    { condition = conds.line_begin }
+  ),
 }
