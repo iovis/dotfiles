db4993725e32f8365654d41ffd9525ed1e53a0d9
David Marchante <davidmarchan@gmail.com>
2025-01-07 15:52:15 -0500

    fix(nvim.plugins.leap): fix leap taking over `;` map

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -1,16 +1,7 @@
 return {
   "ggandor/leap.nvim",
-  event = "VeryLazy",
   config = function()
-    local leap = require("leap")
-
-    leap.setup({})
-
     vim.keymap.set({ "n", "x", "o" }, "f", "<Plug>(leap-forward-to)")
     vim.keymap.set({ "n", "x", "o" }, "F", "<Plug>(leap-backward-to)")
-    -- vim.keymap.set({ "x", "o" }, "t", "<Plug>(leap-forward-till)")
-    -- vim.keymap.set({ "x", "o" }, "T", "<Plug>(leap-backward-till)")
-
-    leap.add_repeat_mappings(";", ",")
   end,
 }
