ff106c3c70969436fc0724093c61f8c17b4833e2
David Marchante <davidmarchan@gmail.com>
2025-01-15 11:42:01 -0500

    feat(hammerspoon.application_launcher): move zoom to home row

--- hammerspoon/.hammerspoon/application_launcher.lua
+++ hammerspoon/.hammerspoon/application_launcher.lua
@@ -1,6 +1,6 @@
 local u = require("utils")
 
-u.bind.app({ ctrl_alt_cmd, "a" }, "Things3")
+u.bind.app({ ctrl_alt_cmd, "a" }, "zoom.us")
 u.bind.app({ ctrl_alt_cmd, "s" }, "Slack")
 u.bind.app({ ctrl_alt_cmd, "d" }, "WezTerm")
 u.bind.app({ ctrl_alt_cmd, "f" }, "Arc")
@@ -10,9 +10,8 @@ u.bind.app({ ctrl_alt_cmd, "g" }, "Google Chrome Canary")
 u.bind.app({ ctrl_alt_cmd, "q" }, "Home")
 u.bind.app({ ctrl_alt_cmd, "w" }, "Messages")
 u.bind.app({ ctrl_alt_cmd, "e" }, "Mail")
-u.bind.app({ ctrl_alt_cmd, "t" }, "TablePlus")
+u.bind.app({ ctrl_alt_cmd, "t" }, "Things3")
 
-u.bind.app({ ctrl_alt_cmd, "z" }, "zoom.us")
 u.bind.app({ ctrl_alt_cmd, "x" }, "Music")
 u.bind.app({ ctrl_alt_cmd, "c" }, "Calendar")
 
