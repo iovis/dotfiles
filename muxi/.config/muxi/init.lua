36a9f106a43482561521bb3de9755ed34c740054
David Marchante <davidmarchan@gmail.com>
2025-02-07 15:20:30 -0500

    feat(muxi): update bindings

--- muxi/.config/muxi/init.lua
+++ muxi/.config/muxi/init.lua
@@ -1,22 +1,11 @@
--- TODO: Add EmmyLua annotations
-
--- TODO: Add to muxi?
-local inspect = require("inspect")
-
-local nvim_opts = "+ZenMode -c 'nmap q <cmd>silent wqa<cr>'"
-local fzf_opts = (
-  [[--bind "ctrl-r:execute(muxi sessions edit -- %s)+reload(muxi sessions list)" ]]
-  .. [[--bind "ctrl-g:execute(muxi config edit -- %s)+reload(muxi sessions list)"]]
-):format(nvim_opts, nvim_opts)
+-- local nvim_opts = "+ZenMode -c 'nmap q <cmd>silent wqa<cr>'"
 
 muxi.uppercase_overrides = true
 muxi.use_current_pane_path = true
 muxi.bindings = {
-  -- TODO: Allow optional `table: 'root|prefix|muxi|...'`
   ["/"] = {
-    command = ("muxi fzf -- %s"):format(fzf_opts),
-    -- command = "muxi sessions switch --tmux-menu",
+    command = "muxi fzf",
   },
 }
 
--- print(inspect(muxi))
+-- print(require("inspect")(muxi))
