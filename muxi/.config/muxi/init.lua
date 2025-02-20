45f35bec8f2533110f06b014e6355e323018fcba
David Marchante <davidmarchan@gmail.com>
2025-02-20 01:17:45 -0500

    feat(muxi): use new fzf and editor config

--- muxi/.config/muxi/init.lua
+++ muxi/.config/muxi/init.lua
@@ -1,11 +1,19 @@
--- local nvim_opts = "+ZenMode -c 'nmap q <cmd>silent wqa<cr>'"
-
 muxi.uppercase_overrides = true
 muxi.use_current_pane_path = true
+muxi.editor_args = {
+  "+ZenMode",
+  "-c",
+  "nmap q <cmd>silent wqa<cr>",
+}
+
+muxi.fzf = {
+  input = false,
+  bind_sessions = false,
+  args = {},
+}
+
 muxi.bindings = {
-  ["/"] = {
-    command = "muxi fzf",
-  },
+  ["/"] = { command = "muxi fzf" },
 }
 
 -- print(require("inspect")(muxi))
