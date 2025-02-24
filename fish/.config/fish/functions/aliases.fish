3244c64ff862144bd3b2633056677f79df578785
David Marchante <davidmarchan@gmail.com>
2025-02-24 10:15:31 -0500

    chore(fish): remove config editing aliases

--- fish/.config/fish/functions/aliases.fish
+++
@@ -1,3 +0,0 @@
-function aliases --wraps='$EDITOR $FDOTDIR/local/aliases.fish' --description 'alias aliases=$EDITOR $FDOTDIR/local/aliases.fish'
-    $EDITOR $FDOTDIR/local/aliases.fish $argv
-end
--- fish/.config/fish/functions/gitconfig.fish
+++
@@ -1,3 +0,0 @@
-function gitconfig --wraps='$EDITOR ~/.config/git/config' --description 'alias gitconfig=$EDITOR ~/.config/git/config'
-    $EDITOR ~/.config/git/config $argv
-end
