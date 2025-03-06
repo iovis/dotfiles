787bbb5ab0b7aa9414f16be972c2bc8fbb1d3bd6
David Marchante <davidmarchan@gmail.com>
2025-03-05 22:04:09 -0500

    feat(fish): update bindings to v4 syntax

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,11 +1,11 @@
 function fish_user_key_bindings
     # NOTE: Use `bind` to check your bindings in the terminal
-    bind \cn accept-autosuggestion
-    bind \cp nextd-or-forward-word
+    bind ctrl-n accept-autosuggestion
+    bind ctrl-p nextd-or-forward-word
 
     # quick command pipes
-    bind \eg '__fish_custom_grep; commandline -f end-of-line'
-    bind \ej '__fish_custom_jq; commandline -f end-of-line'
-    bind \en '__fish_custom_nvim; commandline -f end-of-line'
-    bind \er '__fish_custom_rust_log; commandline -f end-of-line'
+    bind alt-g '__fish_custom_grep; commandline -f end-of-line'
+    bind alt-j '__fish_custom_jq; commandline -f end-of-line'
+    bind alt-n '__fish_custom_nvim; commandline -f end-of-line'
+    bind alt-r '__fish_custom_rust_log; commandline -f end-of-line'
 end
