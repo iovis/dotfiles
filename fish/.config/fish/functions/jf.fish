42d6aa672c6ac24b72166f7d98b41dbb29d53489
David Marchante <davidmarchan@gmail.com>
2025-03-09 16:06:59 -0400

    feat(jf): open in tmux popup to avoid depending on fzf-tmux

--- fish/.config/fish/functions/jf.fish
+++ fish/.config/fish/functions/jf.fish
@@ -1,3 +1,3 @@
-function jf --wraps=just
-    just --choose --chooser "fzf-tmux -p50\%,50\% --prompt 'just> ' --reverse --info inline --preview 'just --show {}' --preview-window 'down'"
+function jf
+    tmux popup -w 75\% -h 50\% -b rounded -T " just " "just --choose --chooser \"fzf --prompt 'just> ' --reverse --info inline-right --header=' ' --no-input --preview 'just --show {}' --preview-window 'right:75%' --bind 'j:down,k:up,q:abort,i,/:show-input+unbind(j,k,q,i,/,x)' --bind 'alt-r:change-preview-window(down|right)'\""
 end
