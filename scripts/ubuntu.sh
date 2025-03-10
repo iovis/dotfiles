c36883f794ea4e466575120b6a6305b295abb39b
David Marchante <davidmarchan@gmail.com>
2025-03-10 17:58:11 -0400

    feat(ubuntu): use jq to extract download url

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -5,9 +5,10 @@
 export DOTFILES="$HOME/.dotfiles"
 export LOCAL_BIN="$HOME/.local/bin"
 export INSTALLATION_DIR="/tmp/install/"
+export PATH=$LOCAL_BIN:$PATH
 
 echo "[$(date '+%Y-%m-%d %H:%M')] Updating Libraries"
-sudo apt-add-repository ppa:fish-shell/release-4
+sudo apt-add-repository -y ppa:fish-shell/release-4
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y build-essential fish jq libffi-dev libfuse2 libyaml-dev libz-dev stow tmux
@@ -18,7 +19,6 @@ git clone https://github.com/iovis/dotfiles "$DOTFILES"
 echo "[$(date '+%Y-%m-%d %H:%M')] Stow"
 cd "$DOTFILES" || exit
 stow fish fzf git lazygit mise muxi nvim starship stylua tmux vim
-cd || exit
 
 echo "[$(date '+%Y-%m-%d %H:%M')] Installing mise"
 curl https://mise.run | sh
@@ -35,27 +35,31 @@ mkdir -p "$INSTALLATION_DIR"
 cd "$INSTALLATION_DIR" || exit
 
 echo "[$(date '+%Y-%m-%d %H:%M')] Installing Lazygit"
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -oP '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+lazygit_version=$(curl -LsSf "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | jq -r '.assets.[].browser_download_url | select(endswith("_Linux_x86_64.tar.gz"))')
+curl -Lo lazygit.tar.gz "$lazygit_version"
 tar xf lazygit.tar.gz lazygit
 install lazygit -D -t "$LOCAL_BIN"
+lazygit --version
 
 echo "[$(date '+%Y-%m-%d %H:%M')] Installing FZF"
-FZF_VERSION=$(curl -s "https://api.github.com/repos/junegunn/fzf/releases/latest" | grep -oP '"tag_name": *"v\K[^"]*')
-curl -Lo fzf.tar.gz "https://github.com/junegunn/fzf/releases/download/v${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz"
+fzf_version=$(curl -LsSf "https://api.github.com/repos/junegunn/fzf/releases/latest" | jq -r '.assets.[].browser_download_url | select(endswith("-linux_amd64.tar.gz"))')
+curl -Lo fzf.tar.gz "$fzf_version"
 tar xf fzf.tar.gz fzf
 install fzf -D -t "$LOCAL_BIN"
+fzf --version
 
 echo "[$(date '+%Y-%m-%d %H:%M')] Installing gh"
-GH_VERSION=$(curl -s "https://api.github.com/repos/cli/cli/releases/latest" | grep -oP '"tag_name": *"v\K[^"]*')
-curl -Lo gh.tar.gz "https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_amd64.tar.gz"
+gh_version=$(curl -LsSf "https://api.github.com/repos/cli/cli/releases/latest" | jq -r '.assets.[].browser_download_url | select(endswith("_linux_amd64.tar.gz"))')
+curl -Lo gh.tar.gz "$gh_version"
 tar xf gh.tar.gz
-install gh_${GH_VERSION}_linux_amd64/bin/gh -D -t "$LOCAL_BIN"
+install gh_*_linux_amd64/bin/gh -D -t "$LOCAL_BIN"
+gh --version
 
 echo "[$(date '+%Y-%m-%d %H:%M')] Installing neovim"
-NVIM_VERSION=$(curl -s "https://api.github.com/repos/cli/cli/releases/latest" | grep -oP '"tag_name": *"v\K[^"]*')
-curl -Lo nvim "https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-linux-x86_64.appimage"
+nvim_version=$(curl -LsSf "https://api.github.com/repos/neovim/neovim/releases/latest" | jq -r '.assets.[].browser_download_url | select(endswith("-linux-x86_64.appimage"))')
+curl -Lo nvim "$nvim_version"
 install nvim -D -t "$LOCAL_BIN"
+nvim --version
 
 echo "[$(date '+%Y-%m-%d %H:%M')] Installation ended"
 cd - || exit
