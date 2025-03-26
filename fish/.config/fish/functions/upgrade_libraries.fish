40130087784228bbabb4761377d5d1a55126b090
David Marchante <davidmarchan@gmail.com>
2025-03-25 21:56:05 -0400

    feat(fish): update 'upgrade_libraries' for multiple package managers

--- fish/.config/fish/functions/upgrade_libraries.fish
+++ fish/.config/fish/functions/upgrade_libraries.fish
@@ -1,6 +1,16 @@
 function upgrade_libraries
-    brew update
-    brew upgrade
-    brew autoremove
-    brewdump
+    if command -q brew
+        brew update
+        brew upgrade
+        brew autoremove
+        brewdump
+    else if command -q pacman
+        sudo pacman -Syu
+    else if command -q apt
+        sudo apt update
+        sudo apt upgrade -y
+    else
+        echo 'Define how to upgrade libraries'
+        exit 1
+    end
 end
