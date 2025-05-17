7b5ec7ca92616f11e67e8fc51c80ca96679bdf9f
David Marchante <davidmarchan@gmail.com>
2025-05-17 01:01:22 -0400

    feat(qutebrowser): change mappings for passthrough

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -105,8 +105,8 @@ table.insert(config.hyperlink_rules, {
 ----Linux
 if wezterm.target_triple ~= "aarch64-apple-darwin" then
   config.font_size = 16
-  config.enable_wayland = false
-  config.front_end = "WebGpu" -- OpenGL, Software, WebGpu
+  -- config.enable_wayland = false
+  -- config.front_end = "WebGpu" -- OpenGL, Software, WebGpu
   config.window_background_opacity = 0.7
 end
 
