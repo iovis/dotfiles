b5534aae3b7a27caeeeeb8553e89bd1be8932045
David Marchante <davidmarchan@gmail.com>
2025-05-23 00:30:16 -0400

    chore(muxi): update settings for muxi

--- muxi/.config/muxi/init.lua
+++ muxi/.config/muxi/init.lua
@@ -1,16 +1,20 @@
 muxi.uppercase_overrides = true
 muxi.use_current_pane_path = true
 
-muxi.editor_args = {
-  "+ZenMode",
-  "-c",
-  "nmap q <cmd>silent wqa<cr>",
+muxi.editor = {
+  args = {
+    "+ZenMode",
+    "-c",
+    "nmap q <cmd>silent wqa<cr>",
+  },
 }
 
 muxi.fzf = {
   input = false,
   bind_sessions = false,
-  args = {},
+  args = {
+    "--bind=alt-/:abort",
+  },
 }
 
 -- muxi.bindings = {
