253bacbb279142d6bcf4a81ca662d3189585924c
David Marchante <davidmarchan@gmail.com>
2025-06-15 15:28:59 -0400

    feat(nvim.plugins.picker): swap neo-tree with snack.picker.explorer

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  enabled = false,
   branch = "v3.x",
   cmd = { "Neotree" },
   keys = {
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -4,12 +4,32 @@ return {
     local snacks = require("snacks")
 
     snacks.setup({
+      explorer = {},
       picker = {
         previewers = {
           -- TODO: fails with mnemonicPrefix
           diff = { cmd = { "delta" } },
         },
         sources = {
+          explorer = {
+            hidden = true,
+            win = {
+              list = {
+                keys = {
+                  [","] = "explorer_up",
+                  ["."] = "explorer_focus",
+                  ["<s-down>"] = "explorer_git_next",
+                  ["<s-up>"] = "explorer_git_prev",
+                  ["<m-j>"] = "preview_scroll_down",
+                  ["<m-k>"] = "preview_scroll_up",
+                  ["<leader>h"] = "edit_split",
+                  ["<leader>t"] = "tab",
+                  ["<leader>v"] = "edit_vsplit",
+                },
+              },
+            },
+          },
+          files = { hidden = true },
           filetypes = {
             name = "filetypes",
             format = "text",
@@ -43,16 +63,23 @@ return {
               ["<c-u>"] = "list_scroll_up",
               ["<esc>"] = { "close", mode = { "n", "i" } },
               ["<m-a>"] = { "select_all", mode = { "n", "i" } },
-              ["<m-cr>"] = { { "pick_win", "jump" }, mode = { "n", "i" } },
               ["<m-j>"] = { "preview_scroll_down", mode = { "i", "n" } },
               ["<m-k>"] = { "preview_scroll_up", mode = { "i", "n" } },
               ["<m-q>"] = { "qflist", mode = { "i", "n" } },
+              ---@diagnostic disable-next-line: assign-type-mismatch
+              ["<m-cr>"] = { { "pick_win", "jump" }, mode = { "n", "i" } },
             },
           },
         },
       },
     })
 
+    ----Explorer
+    vim.keymap.set("n", "<leader>k", function()
+      snacks.explorer()
+    end, { desc = "snacks.explorer" })
+
+    ----Picker
     vim.keymap.set("n", "s<space>", function()
       snacks.picker()
     end, { desc = "snacks.picker" })
@@ -63,6 +90,7 @@ return {
     vim.keymap.set("n", "<leader>fm", snacks.picker.man, { desc = "snacks.picker.man" })
     vim.keymap.set("n", "<leader>fq", snacks.picker.qflist, { desc = "snacks.picker.qflist" })
     vim.keymap.set("n", "<leader>j", snacks.picker.git_status, { desc = "snacks.picker.git_status" })
+    vim.keymap.set("n", "<leader>o", snacks.picker.files, { desc = "snacks.picker.files" })
     vim.keymap.set("n", "<leader>r", snacks.picker.resume, { desc = "snacks.picker.resume" })
     vim.keymap.set("n", "U", snacks.picker.undo, { desc = "snacks.picker.undo" })
     vim.keymap.set("n", "gm", snacks.picker.buffers, { desc = "snacks.picker.buffers" })
@@ -71,10 +99,6 @@ return {
     vim.keymap.set("n", "<leader>F", snacks.picker.filetypes, { desc = "snacks.picker.filetypes" })
 
     -- Files
-    vim.keymap.set("n", "<leader>o", function()
-      snacks.picker.files({ hidden = true })
-    end, { desc = "Find Files" })
-
     vim.keymap.set("n", "<leader>ud", function()
       snacks.picker.files({ cwd = vim.fn.stdpath("config") })
     end, { desc = "Open Neovim config" })
@@ -97,21 +121,10 @@ return {
     end, { desc = "snacks.picker.grep" })
 
     -- LSP
-    vim.keymap.set("n", "<leader>ls", snacks.picker.lsp_symbols, { desc = "snacks.picker.lsp_symbols" })
-    vim.keymap.set(
-      "n",
-      "<leader>lw",
-      snacks.picker.lsp_workspace_symbols,
-      { desc = "snacks.picker.lsp_workspace_symbols" }
-    )
-    vim.keymap.set(
-      "n",
-      "<leader>lw",
-      snacks.picker.lsp_workspace_symbols,
-      { desc = "snacks.picker.lsp_workspace_symbols" }
-    )
     vim.keymap.set("n", "<leader>ld", snacks.picker.diagnostics, { desc = "snacks.picker.diagnostics" })
-
-    -- { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+    vim.keymap.set("n", "<leader>ls", snacks.picker.lsp_symbols, { desc = "snacks.picker.lsp_symbols" })
+    vim.keymap.set("n", "<leader>lw", snacks.picker.lsp_workspace_symbols, {
+      desc = "snacks.picker.lsp_workspace_symbols",
+    })
   end,
 }
