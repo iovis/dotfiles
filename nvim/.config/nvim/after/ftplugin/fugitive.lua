7b3f5db070e22cfb31741ca4b73619128e8d8788
David Marchante <davidmarchan@gmail.com>
2025-06-18 11:41:59 -0400

    feat(nvim.ft.fugitive): normal bindings for opening in splits

--- nvim/.config/nvim/after/ftplugin/fugitive.lua
+++ nvim/.config/nvim/after/ftplugin/fugitive.lua
@@ -3,3 +3,19 @@ vim.keymap.set("n", "q", "gq", {
   nowait = true,
   remap = true,
 })
+
+-- open in splits
+vim.keymap.set("n", "<c-s>", "o", {
+  buffer = true,
+  remap = true,
+})
+
+vim.keymap.set("n", "<c-v>", "gO", {
+  buffer = true,
+  remap = true,
+})
+
+vim.keymap.set("n", "<c-t>", "O", {
+  buffer = true,
+  remap = true,
+})
