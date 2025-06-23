b001934bf9b712e94c9f2e6eca57a85387b2622c
David Marchante <davidmarchan@gmail.com>
2025-06-23 16:39:31 -0400

    feat(nvim.plugins.quicker): adjust filename width

--- nvim/.config/nvim/lua/plugins/quicker.lua
+++ nvim/.config/nvim/lua/plugins/quicker.lua
@@ -24,8 +24,31 @@ return {
         H = "",
       },
       max_filename_width = function()
-        -- return math.floor(math.min(95, vim.o.columns / 4))
-        return math.huge
+        if vim.g.quicker_expanded then
+          return math.huge
+        else
+          return math.floor(math.min(95, vim.o.columns / 4))
+        end
+      end,
+    })
+
+    -- https://github.com/stevearc/quicker.nvim/issues/54
+    local augroup = vim.api.nvim_create_augroup("quicker_bindings", { clear = true })
+    vim.g.quicker_expanded = true
+
+    vim.api.nvim_create_autocmd("FileType", {
+      desc = "quicker.nvim bindings",
+      group = augroup,
+      pattern = { "qf" },
+      callback = function(event)
+        vim.keymap.set("n", "yoe", function()
+          -- It creates a new quickfix, but at least it's properly formatted
+          vim.g.quicker_expanded = not vim.g.quicker_expanded
+          vim.fn.setqflist(vim.fn.getqflist())
+        end, {
+          desc = "Toggle quicker.nvim filename width",
+          buffer = event.buf,
+        })
       end,
     })
   end,
