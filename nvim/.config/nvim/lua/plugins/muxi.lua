93931bbfe9ee5b03007c7a9b432eefaef0fe2c89
David Marchante <davidmarchan@gmail.com>
2025-07-03 21:59:06 -0400

    feat(nvim.plugins.muxi): start snacks picker filtering by key

--- nvim/.config/nvim/lua/plugins/muxi.lua
+++ nvim/.config/nvim/lua/plugins/muxi.lua
@@ -55,7 +55,7 @@ return {
     ----Mark management
     vim.keymap.set("n", "g/", function()
       ---@diagnostic disable-next-line: undefined-field
-      Snacks.picker.muxi_marks()
+      Snacks.picker.muxi_marks({ pattern = "key:" })
     end, { desc = "snacks.picker.muxi_marks" })
 
     vim.keymap.set("n", "m/", muxi.ui.qf, {
