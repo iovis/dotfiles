13ca406962a72a29d882ecf21a4a4ba387c1449e
David Marchante <davidmarchan@gmail.com>
2025-07-11 22:51:08 -0400

    feat(nvim.lsp.lua_ls): disable inlay hints for array indexes

--- nvim/.config/nvim/after/lsp/lua_ls.lua
+++ nvim/.config/nvim/after/lsp/lua_ls.lua
@@ -7,7 +7,10 @@ return {
         unusedLocalExclude = { "_*" }, -- Don't warn about variables that start with underscore
       },
       format = { enable = false },
-      hint = { enable = true },
+      hint = {
+        enable = true,
+        arrayIndex = "Disable",
+      },
       -- workspace = { checkThirdParty = false },
     },
   },
