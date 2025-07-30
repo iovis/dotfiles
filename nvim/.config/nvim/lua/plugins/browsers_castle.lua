52d359d30f9aad1cc6fa991fa447b514bd6b1eb1
David Marchante <davidmarchan@gmail.com>
2025-07-30 16:41:44 -0400

    feat(nvim.plugins.browsers_castle): don't map over `go`
    I don't really use this plugin anyway...

--- nvim/.config/nvim/lua/plugins/browsers_castle.lua
+++ nvim/.config/nvim/lua/plugins/browsers_castle.lua
@@ -2,9 +2,6 @@ return {
   "iovis/browsers_castle",
   event = "VeryLazy",
   config = function()
-    vim.keymap.set("n", "go<space>", ":Google<space>")
-    vim.keymap.set("n", "go<cr>", ":Google <c-r><c-w><cr>")
-
     vim.keymap.set("x", "go<space>", 'y:Google <c-r>"')
     vim.keymap.set("x", "go<cr>", 'y:Google <c-r>"<cr>')
   end,
