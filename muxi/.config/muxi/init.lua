2abf20158b00bc9b9e68bad004699bac4ac8c6b9
David Marchante <davidmarchan@gmail.com>
2025-10-04 19:46:06 -0400

    feat(muxi): update settings to new syntax

--- muxi/.config/muxi/init.lua
+++ muxi/.config/muxi/init.lua
@@ -1,22 +1,16 @@
-muxi.uppercase_overrides = true
-muxi.use_current_pane_path = true
-
-muxi.editor = {
-  args = {
-    "+ZenMode",
-    "-c",
-    "nmap q <cmd>silent wqa<cr>",
-  },
-}
-
-muxi.fzf = {
-  input = false,
-  bind_sessions = false,
-  args = {},
-}
-
--- muxi.bindings = {
---   ["/"] = { command = "muxi fzf" },
--- }
-
 -- print(require("inspect")(muxi))
+
+return {
+  uppercase_overrides = true,
+  use_current_pane_path = true,
+  editor = {
+    args = { "+ZenMode", "-c", "nmap q <cmd>silent wqa<cr>" },
+  },
+  fzf = {
+    input = false,
+    bind_sessions = false,
+  },
+  -- bindings = {
+  --   ["/"] = { command = "muxi fzf" },
+  -- },
+}
