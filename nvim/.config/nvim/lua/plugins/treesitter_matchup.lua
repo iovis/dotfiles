5667db3bc699a2ebc259f6de4f1595f147ac963f
David Marchante <davidmarchan@gmail.com>
2025-10-07 19:07:30 -0400

    feat(nvim.plugins.vim-matchup): disable default mappings

--- nvim/.config/nvim/lua/plugins/treesitter_matchup.lua
+++ nvim/.config/nvim/lua/plugins/treesitter_matchup.lua
@@ -1,7 +1,10 @@
 return {
   "andymass/vim-matchup",
   init = function()
+    vim.g.matchup_mappings_enabled = 0
     vim.g.matchup_matchparen_offscreen = {}
     vim.g.matchup_treesitter_disable_virtual_text = true
+
+    vim.keymap.set({ "n", "x", "o" }, "%", "<plug>(matchup-%)")
   end,
 }
