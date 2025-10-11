4ba49c3324f06ce387927a2c256ba02f09a034ab
David Marchante <davidmarchan@gmail.com>
2025-10-10 22:47:04 -0400

    feat(fish): set default for glg to last 5 commits

--- fish/.config/fish/functions/glg.fish
+++ fish/.config/fish/functions/glg.fish
@@ -1,3 +1,7 @@
 function glg --wraps="git log"
+    if not set -q argv[1]
+        set argv -5
+    end
+
     git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' $argv
 end
