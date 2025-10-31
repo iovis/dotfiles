6292121c94f604bfeb1907b9a4a32b374506e0f3
David Marchante <davidmarchan@gmail.com>
2025-10-31 00:46:52 -0400

    feat(nvim.lsp.rust_analyzer): simplify clippy args

--- fish/.config/fish/functions/clippy.fish
+++ fish/.config/fish/functions/clippy.fish
@@ -1,7 +1,3 @@
 function clippy
-    cargo clippy -- -W clippy::pedantic \
-        -A clippy::missing-errors-doc \
-        -A clippy::missing-panics-doc \
-        -A clippy::must-use-candidate \
-        -A clippy::needless_range_loop
+    cargo clippy -- -W clippy::pedantic -A clippy::missing-errors-doc -A clippy::missing-panics-doc
 end
