a367cec389f90dbe9340fc7b0d18201550dd2a3e
David Marchante <davidmarchan@gmail.com>
2025-10-31 16:52:03 -0400

    feat(muxi): move plugin configs to muxi

--- muxi/.config/muxi/init.lua
+++ muxi/.config/muxi/init.lua
@@ -2,14 +2,40 @@
 
 return {
   use_current_pane_path = true,
-  plugins = {
-    "tmux-plugins/tmux-continuum",
-    "tmux-plugins/tmux-cpu",
-    "tmux-plugins/tmux-resurrect",
-    "tmux-plugins/tmux-yank",
-  },
-  editor = {
-    args = { "+ZenMode", "-c", "nmap q <cmd>silent wqa<cr>" },
-  },
   fzf = { input = false },
+  editor = {
+    args = { "-c", "nmap q <cmd>silent wqa<cr>" },
+  },
+  plugins = {
+    {
+      url = "tmux-plugins/tmux-resurrect",
+      opts = {
+        ["resurrect-capture-pane-contents"] = "on",
+        ["resurrect-processes"] = "lazygit",
+        ["resurrect-strategy-nvim"] = "session",
+      },
+    },
+    {
+      url = "tmux-plugins/tmux-continuum",
+      opts = {
+        -- ["continuum-restore"] = "on", -- Seems to cause issues when resurrecting (race condition?)
+        ["continuum-save-interval"] = "5",
+      },
+    },
+    {
+      url = "tmux-plugins/tmux-cpu",
+      opts = {
+        cpu_low_fg_color = "#[fg=#51576d]",
+        cpu_medium_fg_color = "#[fg=#e5c890]",
+        cpu_high_fg_color = "#[fg=#e78284]",
+      },
+    },
+    {
+      url = "tmux-plugins/tmux-yank",
+      opts = {
+        copy_mode_put = "Space",
+        yank_selection_mouse = "clipboard",
+      },
+    },
+  },
 }
