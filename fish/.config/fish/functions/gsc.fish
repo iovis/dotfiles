6ddd81180e45bc30fbce58eb66f163e2f9278c11
David Marchante <davidmarchan@gmail.com>
2025-12-30 23:31:52 -0500

    feat(arch): remove mangohud and gamescope

--- fish/.config/fish/functions/gsc.fish
+++
@@ -1,6 +0,0 @@
-function gsc
-    # Set gamescope priority
-    # sudo setcap 'CAP_SYS_NICE=eip' $(which gamescope)
-    # gamescope -W 2560 -H 1440 --fullscreen --force-grab-cursor --expose-wayland -- %command%
-    gamescope -W 2560 -H 1440 -r 360 --fullscreen --force-grab-cursor --expose-wayland -- $argv
-end
