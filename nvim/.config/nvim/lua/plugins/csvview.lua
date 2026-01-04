656a94b54f327bde6707724b563ca4bdc6755de1
David Marchante <davidmarchan@gmail.com>
2026-01-03 22:48:57 -0500

    feat(nvim.plugins.csvview): update keymaps and view settings

--- nvim/.config/nvim/lua/plugins/csvview.lua
+++ nvim/.config/nvim/lua/plugins/csvview.lua
@@ -2,10 +2,13 @@ return {
   "hat0uma/csvview.nvim",
   config = function()
     require("csvview").setup({
-      view = { display_mode = "border" },
+      view = {
+        min_column_width = 3,
+        display_mode = "border",
+      },
       keymaps = {
-        textobject_field_inner = { "if", mode = { "o", "x" } },
-        textobject_field_outer = { "af", mode = { "o", "x" } },
+        textobject_field_inner = { "ij", mode = { "o", "x" } },
+        textobject_field_outer = { "aj", mode = { "o", "x" } },
         jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
         jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
         jump_next_row = { "<Enter>", mode = { "n", "v" } },
