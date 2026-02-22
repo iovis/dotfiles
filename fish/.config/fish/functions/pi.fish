ea956deb22e7c886b196e90a47e5ee7ddf243f12
David Marchante <davidmarchan@gmail.com>
2026-02-22 14:17:40 -0500

    chore(arch): clean up packages
    Marked some dependencies that were marked as explicit with:
    
    sudo pacman -D --asdeps package
    
    Also removed some that I don't need anymore

--- fish/.config/fish/functions/pi.fish
+++ fish/.config/fish/functions/pi.fish
@@ -1,3 +1,11 @@
 function pi --wraps="yay -Sii"
+    log_step $(green $argv) info
     yay -Sii $argv
+
+    log_step Installed executables
+    pacman -Qlq $argv | while read -l f
+        if test -f "$f"; and test -x "$f"
+            echo "$f"
+        end
+    end
 end
