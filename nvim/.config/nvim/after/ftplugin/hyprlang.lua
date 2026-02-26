11c543c70a079ca3930b93aeeb630e913c66f474
David Marchante <davidmarchan@gmail.com>
2026-02-26 00:17:29 -0500

    feat(hypridle): turn screen off at 10 minutes

--- nvim/.config/nvim/after/ftplugin/hyprlang.lua
+++ nvim/.config/nvim/after/ftplugin/hyprlang.lua
@@ -1,2 +1,7 @@
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
+
+local u = require("config.utils")
+if u.current_file():match("hypridle.conf") then
+  vim.keymap.set("n", "s<cr>", "<cmd>Tux systemctl --user restart hypridle.service<cr>", { buffer = true })
+end
