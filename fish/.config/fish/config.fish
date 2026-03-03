9de5e35af273c6db9e19398f788e1d5f0104d2d3
David Marchante <davidmarchan@gmail.com>
2026-03-03 18:33:15 -0500

    feat(fish): expand brew shellenv because it's slow af

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -42,7 +42,20 @@ set -gx JUST_CHOOSER '\
 
 ## PATH
 if test -e /opt/homebrew/bin/brew
-    /opt/homebrew/bin/brew shellenv | source
+    # /opt/homebrew/bin/brew shellenv | source
+    set --global --export HOMEBREW_PREFIX /opt/homebrew
+    set --global --export HOMEBREW_CELLAR /opt/homebrew/Cellar
+    set --global --export HOMEBREW_REPOSITORY /opt/homebrew
+
+    fish_add_path --global --move --path /opt/homebrew/bin /opt/homebrew/sbin
+
+    if test -n "$MANPATH[1]"
+        set --global --export MANPATH '' $MANPATH
+    end
+
+    if not contains /opt/homebrew/share/info $INFOPATH
+        set --global --export INFOPATH /opt/homebrew/share/info $INFOPATH
+    end
 end
 
 fish_add_path $HOME/.dotfiles/bin
