8d8adf2b7b392b24193b06e186927733200ab754
David Marchante <davidmarchan@gmail.com>
2026-03-19 11:51:48 -0400

    feat(ghostty): add ghostty settings

--- nvim/.config/nvim/after/ftplugin/conf.lua
+++ nvim/.config/nvim/after/ftplugin/conf.lua
@@ -10,4 +10,25 @@ if u.current_file():match("kitty.conf") then
       vim.cmd("silent !killall -SIGUSR1 kitty")
     end,
   })
+elseif u.current_file():match("config.ghostty") then
+  local augroup = vim.api.nvim_create_augroup("ghostty_reload", { clear = true })
+  vim.api.nvim_create_autocmd("BufWritePost", {
+    desc = "Reload ghostty config on save",
+    group = augroup,
+    buffer = vim.api.nvim_get_current_buf(),
+    callback = function()
+      vim.cmd("silent !killall -SIGUSR2 ghostty")
+    end,
+  })
+
+  vim.keymap.set("n", "s?", function()
+    vim.cmd("vnew")
+    vim.cmd("0r !ghostty +show-config --default --docs")
+    vim.cmd.normal("gg")
+    vim.bo.filetype = "conf"
+    vim.keymap.set("n", "q", "<cmd>close<cr>", {
+      buffer = true,
+      nowait = true,
+    })
+  end, { buffer = true })
 end
