c20b41dea33c7c03922dfa5c45d3fbb30914984e
David Marchante <davidmarchan@gmail.com>
2026-04-14 19:47:12 -0400

    chore(nvim.plugins.tux): cleanup

--- nvim/.config/nvim/lua/plugins/tux.lua
+++ nvim/.config/nvim/lua/plugins/tux.lua
@@ -4,30 +4,24 @@ return {
   event = "VeryLazy",
   config = function()
     local tux = require("tux")
-
     tux.setup({})
 
     local u = require("config.utils")
     u.ex.abbrev("tux", "Tux")
 
     vim.keymap.set("n", "c<space>", ":Tux<space>")
-    vim.keymap.set("n", "<leader>i", ":Tux Up<cr>", {
-      desc = "[tux] repeat last tmux command",
-      silent = true,
-    })
+    vim.keymap.set("n", "<leader>i", "<cmd>Tux Up<cr>")
+    vim.keymap.set("n", "s<cr>", "<cmd>Tux just run<cr>")
+    vim.keymap.set("n", "m<cr>", "<cmd>Tux just build<cr>")
+    vim.keymap.set("n", "<leader>I", ":.Tux<cr>", { desc = "[tux] execute current line" })
+    vim.keymap.set("x", "<leader>i", ":Tux<cr>", { desc = "[tux] run selected lines" })
 
-    local function run_justfile_task_picker()
+    vim.keymap.set("n", "<leader>S", function()
       if u.has_justfile() then
         tux.run("just --choose")
       else
         vim.notify("No justfile found", vim.log.levels.WARN)
       end
-    end
-
-    vim.keymap.set("n", "s<cr>", "<cmd>Tux just run<cr>", { desc = "[tux] just run" })
-    vim.keymap.set("n", "m<cr>", "<cmd>Tux just build<cr>", { desc = "[tux] just build" })
-    vim.keymap.set("n", "<leader>S", run_justfile_task_picker, { desc = "[tux] run a task from the Justfile" })
-    vim.keymap.set("n", "<leader>I", ":.Tux<cr>", { desc = "[tux] execute current line" })
-    vim.keymap.set("x", "<leader>i", ":Tux<cr>", { desc = "[tux] run selected lines" })
+    end, { desc = "[tux] run a task from the Justfile" })
   end,
 }
