75aeb4e4bda98507d1a992bbf267318318d0aca7
David Marchante <davidmarchan@gmail.com>
2026-04-22 20:50:35 -0400

    feat(nvim.ui2): use ui2

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,13 +12,11 @@ require("config.ui.statusline")
 require("config.lsp")
 
 -- Experimental new messages UI
-if vim.g.use_ui2 then
-  require("vim._core.ui2").enable({})
-  vim.keymap.set("n", "<leader>M", "<cmd>messages<cr>")
-  vim.keymap.set("n", "g,", "g<", {
-    desc = "Show latest messages in the pager",
-  })
-end
+require("vim._core.ui2").enable({})
+vim.keymap.set("n", "<leader>M", "<cmd>messages<cr>")
+vim.keymap.set("n", "g,", "g<", {
+  desc = "Show latest messages in the pager",
+})
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
