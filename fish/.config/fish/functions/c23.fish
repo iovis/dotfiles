f377a5fbebec11fa7457b524b849946d78365d61
David Marchante <davidmarchan@gmail.com>
2026-05-15 14:45:06 -0400

    feat(nvim.ft.c): update C ftplugin

--- fish/.config/fish/functions/c23.fish
+++
@@ -1,3 +0,0 @@
-function c23 --wraps=clang
-    /opt/homebrew/opt/llvm/bin/clang -std=c23 $argv
-end
--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -11,16 +11,6 @@ if u.current_file():match("ext/") then
   vim.keymap.set("n", "m<cr>", "<cmd>Tux bear -- rake<cr>", { buf = 0 })
   vim.keymap.set("n", "s<cr>", "<cmd>Tux bear -- rake<cr>", { buf = 0 })
   vim.keymap.set("n", "<leader>sw", "<cmd>Tux watchexec -e c,h,rb -c clear -- bear -- rake<cr>", { buf = 0 })
-elseif u.current_file():match("_test.c$") then
-  vim.keymap.set("n", "<leader>sa", "<cmd>Tux just test<cr>", { buf = 0 })
-  vim.keymap.set("n", "m<cr>", "<cmd>Tux just test<cr>", { buf = 0 })
-
-  local suite_name = u.current_file():match("(%w+)_test.c$")
-  vim.keymap.set("n", "<leader>so", ("<cmd>Tux just test --filter '%s/*'<cr>"):format(suite_name), { buf = 0 })
-  vim.keymap.set("n", "s<cr>", ("<cmd>Tux just test --filter '%s/*'<cr>"):format(suite_name), { buf = 0 })
-
-  -- TODO: use treesitter to extract the suite_name/test_name
-  vim.keymap.set("n", "<leader>si", "<cmd>Tux just test --filter " .. "<cr>", { buf = 0 })
 elseif vim.uv.cwd():match("qmk_userspace") then
   if u.current_file():match("keyboards/") then
     vim.keymap.set("n", "<leader>S", function()
@@ -56,11 +46,15 @@ elseif vim.uv.cwd():match("qmk_userspace") then
     })
   end, { buf = 0 })
 elseif u.has_justfile() then
-  vim.keymap.set("n", "s<cr>", "<cmd>Tux just run<cr>", { buf = 0 })
+  vim.keymap.set("n", "d<cr>", "<cmd>Tux just debug<cr>", { buf = 0 })
   vim.keymap.set("n", "m<cr>", "<cmd>Tux just build<cr>", { buf = 0 })
+  vim.keymap.set("n", "s<cr>", "<cmd>Tux just run<cr>", { buf = 0 })
 
-  vim.keymap.set("n", "<leader>sw", "<cmd>Tux w -e c,h -c clear just run<cr>", { buf = 0 })
+  vim.keymap.set("n", "<leader>so", "<cmd>Tux just test<cr>", { buf = 0 })
+  vim.keymap.set("n", "<leader>sw", "<cmd>Tux just watch<cr>", { buf = 0 })
+  vim.keymap.set("n", "<leader>st", "<cmd>Tux just watch_test<cr>", { buf = 0 })
 else
-  vim.keymap.set("n", "s<cr>", "<cmd>Tux c23 -o %:.:r %:. && ./%:.:r && rm %:.:r<cr>", { buf = 0 })
-  vim.keymap.set("n", "m<cr>", "<cmd>Tux c23 -o %:.:r %:.<cr>", { buf = 0 })
+  local cc = "clang -std=c23 -fdefer-ts -Wall -Wextra -Wpedantic -Werror -O3"
+  vim.keymap.set("n", "s<cr>", "<cmd>Tux " .. cc .. " -o %:t:r %:. && ./%:t:r && rm %:t:r<cr>", { buf = 0 })
+  vim.keymap.set("n", "m<cr>", "<cmd>Tux " .. cc .. " -o %:t:r %:.<cr>", { buf = 0 })
 end
