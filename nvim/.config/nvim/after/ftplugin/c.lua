73c6a2b2c5cd5ede53ad5150da95a07660eb0a50
David Marchante <davidmarchan@gmail.com>
2026-05-22 15:36:44 -0400

    feat(nvim.ft.c): more runner tweaks

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -12,14 +12,6 @@ if u.current_file():match("ext/") then
   vim.keymap.set("n", "s<cr>", "<cmd>Tux bear -- rake<cr>", { buf = 0 })
   vim.keymap.set("n", "<leader>sw", "<cmd>Tux watchexec -e c,h,rb -c clear -- bear -- rake<cr>", { buf = 0 })
 elseif vim.uv.cwd():match("qmk_userspace") then
-  if u.current_file():match("keyboards/") then
-    vim.keymap.set("n", "<leader>S", function()
-      -- keyboards/(boardsource/unicorne)/keymaps/...
-      local keyboard = u.current_file():match([[keyboards/(.*)/keymaps]])
-      vim.cmd.FloatTerm("just choose " .. keyboard)
-    end, { buf = 0 })
-  end
-
   vim.keymap.set("n", "s<cr>", function()
     vim.cmd.FloatTerm("just run")
   end, { buf = 0 })
@@ -29,7 +21,7 @@ elseif vim.uv.cwd():match("qmk_userspace") then
   end, { buf = 0 })
 
   vim.keymap.set("n", "<leader>do", "<cmd>botright split! ../qmk_firmware/docs/keycodes.md<cr>", { buf = 0 })
-  vim.keymap.set("n", "<leader>dS", "<cmd>60vs keymap.md<cr>", { buf = 0 })
+  vim.keymap.set("n", "d<cr>", "<cmd>60vs keymap.md<cr>", { buf = 0 })
 
   vim.keymap.set("n", "s?", function()
     vim.notify(u.system({
@@ -37,13 +29,6 @@ elseif vim.uv.cwd():match("qmk_userspace") then
       "config",
       "user.keyboard",
     }))
-  end, { buf = 0 })
-
-  vim.keymap.set("n", "d<cr>", function()
-    tux.window("qmk cd", {
-      name = "firmware",
-      select = true,
-    })
   end, { buf = 0 })
 elseif u.has_justfile() then
   vim.keymap.set("n", "m<cr>", "<cmd>Tux just build<cr>", { buf = 0 })
@@ -64,7 +49,19 @@ elseif u.has_justfile() then
     tux.pane(debug_cmd, { focus = true })
   end, { buf = 0, desc = "Test debug current line" })
 else
-  local cc = "clang -std=c23 -fdefer-ts -Wall -Wextra -Wpedantic -Werror -O3"
+  local cc = "clang -std=c23 -fdefer-ts -Wall -Wextra -Wpedantic -O3"
   vim.keymap.set("n", "s<cr>", "<cmd>Tux " .. cc .. " -o %:t:r %:. && ./%:t:r && rm %:t:r<cr>", { buf = 0 })
   vim.keymap.set("n", "m<cr>", "<cmd>Tux " .. cc .. " -o %:t:r %:.<cr>", { buf = 0 })
+
+  vim.keymap.set("n", "d<cr>", function()
+    local clang = "clang -std=c23 -fdefer-ts -Wall -Wextra -Wpedantic -g -O0"
+    local program_name = vim.fn.expand("%:t:r")
+    local file_path = vim.fn.expand("%:.")
+
+    local compile_debug = ("%s -o %s %s"):format(clang, program_name, file_path)
+    local run_debug = ('ASAN_OPTIONS=detect_leaks=0 lldb -o "b main" -o "run" -- ./%s'):format(program_name)
+
+    tux.pane(compile_debug)
+    tux.pane(run_debug, { focus = true })
+  end, { buf = 0, desc = "Debug program" })
 end
