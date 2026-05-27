5b272df1d539449d07ba701f345e22c4f6ea8fb0
David Marchante <davidmarchan@gmail.com>
2026-05-27 17:38:16 -0400

    chore(nvim.plugins.tux): clarify binding description

--- nvim/.config/nvim/lua/plugins/tux.lua
+++ nvim/.config/nvim/lua/plugins/tux.lua
@@ -24,11 +24,10 @@ return {
 
     if u.has_justfile() then
       vim.keymap.set("n", "<leader>st", "<cmd>Tux just test<cr>")
-
-      vim.keymap.set("n", "<leader>sj", "<cmd>Tux just<cr>", { desc = "Tux just (run default recipe)" })
-      vim.keymap.set("n", "<leader>sk", "<cmd>Tux just run<cr>")
       vim.keymap.set("n", "<leader>sm", "<cmd>Tux just build<cr>")
 
+      vim.keymap.set("n", "<leader>sj", "<cmd>Tux just<cr>", { desc = "[tux] just (default recipe)" })
+      vim.keymap.set("n", "<leader>sk", "<cmd>Tux just run<cr>")
       vim.keymap.set("n", "<leader>sl", function()
         tux.pane("just console", { focus = true })
       end, { desc = "Tux just console" })
