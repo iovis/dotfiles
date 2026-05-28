641b8309a016bcb0f75ef2ffbacb562ca7fc4043
David Marchante <davidmarchan@gmail.com>
2026-05-28 02:08:17 -0400

    feat(nvim.plugins.fugitive): tweak fugitive bindings

--- nvim/.config/nvim/after/ftplugin/fugitive.lua
+++ nvim/.config/nvim/after/ftplugin/fugitive.lua
@@ -1,21 +1,7 @@
-vim.keymap.set("n", "q", "gq", {
-  buf = 0,
-  nowait = true,
-  remap = true,
-})
+vim.keymap.set("n", "q", "gq", { buf = 0, nowait = true, remap = true })
+vim.keymap.set("n", "dd", "X", { buf = 0, remap = true })
 
 -- open in splits
-vim.keymap.set("n", "<c-s>", "o", {
-  buf = 0,
-  remap = true,
-})
-
-vim.keymap.set("n", "<c-v>", "gO", {
-  buf = 0,
-  remap = true,
-})
-
-vim.keymap.set("n", "<c-t>", "O", {
-  buf = 0,
-  remap = true,
-})
+vim.keymap.set("n", "<c-s>", "o", { buf = 0, remap = true })
+vim.keymap.set("n", "<c-v>", "gO", { buf = 0, remap = true })
+vim.keymap.set("n", "<c-t>", "O", { buf = 0, remap = true })
