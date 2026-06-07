488f1002de19fdd18ecd6e7dbe313a2cd566b691
David Marchante <davidmarchan@gmail.com>
2026-06-07 18:46:14 -0400

    feat(nvim.colors.cursorline): extend cursorline through statuscol

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -22,6 +22,8 @@ return {
         return {
           -- general
           CursorLine = { bg = black },
+          CursorLineSign = { link = "CursorLine" },
+          CursorLineNr = { fg = colors.lavender, bg = black },
           EndOfBuffer = {},
           StatusLine = {},
           StatusLineNC = {},
