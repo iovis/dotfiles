bc075a605e73dea48ac329d76d6426f2a8434677
David Marchante <davidmarchan@gmail.com>
2026-06-21 13:19:18 -0400

    fix(hyprland.autostart): fix swaync trying to start twice

--- hyprland/.config/hypr/config/autostart.lua
+++ hyprland/.config/hypr/config/autostart.lua
@@ -1,12 +1,14 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 hl.on("hyprland.start", function()
-  -- hl.exec_cmd("systemctl --user enable --now hypridle.service")
-  hl.exec_cmd("systemctl --user enable --now hyprpaper.service")
+  hl.exec_cmd("systemctl --user start hyprpaper.service")
   hl.exec_cmd("systemctl --user start hyprpolkitagent")
-  hl.exec_cmd("systemctl enable --now --user vicinae.service")
-  hl.exec_cmd("uwsm app -- hypridle")
+  hl.exec_cmd("systemctl --user start swaync.service")
+  hl.exec_cmd("systemctl --user start vicinae.service")
+
   hl.exec_cmd("GTK_THEME=Adwaita:dark uwsm app -- swayosd-server")
-  hl.exec_cmd("uwsm app -- swaync")
   hl.exec_cmd("uwsm app -- udiskie")
   hl.exec_cmd("uwsm app -- waybar")
+
+  -- Don't run as a service to be able to control inhibit
+  hl.exec_cmd("uwsm app -- hypridle")
 end)
