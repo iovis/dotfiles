fe6182eb189b1439db767136740ae08a0620f1d0
David Marchante <davidmarchan@gmail.com>
2026-07-01 01:43:06 -0400

    feat(nvim.plugins): remove vim-dadbod

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -5,6 +5,5 @@ vim.cmd("packadd! termdebug")
 
 return {
   { "dstein64/vim-startuptime", cmd = "StartupTime" },
-  { "tpope/vim-dadbod", cmd = { "DB" }, keys = { { "d<space>", ":DB<space>" } } },
   { "vim-test/vim-test", event = "VeryLazy" },
 }
