d4e9976755626ede249ec92cbcc8171d375e3a54
David Marchante <davidmarchan@gmail.com>
2026-07-05 14:59:47 -0400

    chore(nvim.ftplugin.sh): clean up yabai mentions

--- nvim/.config/nvim/after/ftplugin/sh.lua
+++ nvim/.config/nvim/after/ftplugin/sh.lua
@@ -1,24 +1,2 @@
-local u = require("config.utils")
-local tux = require("tux")
-
 vim.keymap.set("n", "s<cr>", "<cmd>Tux %<cr>", { buf = 0 })
 vim.keymap.set("n", "m<cr>", "<cmd>!chmod +x %<cr>", { buf = 0 })
-
----- yabai
-if u.current_file():match("yabai/") then
-  vim.keymap.set("n", "s<cr>", function()
-    tux.window("yabai --restart-service", {
-      detached = true,
-      select = false,
-      name = nil,
-    })
-  end, { buf = 0 })
-
-  vim.keymap.set("n", "<leader>so", function()
-    tux.window("yabai --restart-service", {
-      detached = true,
-      select = false,
-      name = nil,
-    })
-  end, { buf = 0 })
-end
