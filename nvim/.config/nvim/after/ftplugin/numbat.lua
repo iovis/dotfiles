c86eaa74377d4ff45a3bb071a70aeb6953b70c71
David Marchante <davidmarchan@gmail.com>
2026-07-05 15:00:01 -0400

    fix(nvim.ftplugin.numbat): fix open docs binding

--- nvim/.config/nvim/after/ftplugin/numbat.lua
+++ nvim/.config/nvim/after/ftplugin/numbat.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "s<cr>", "<cmd>Tux numbat %:.<cr>", { buf = 0 })
-vim.keymap.set("n", "d<cr>", "<cmd>silent !open https://numbat.dev/doc/<cr>", { buf = 0 })
+vim.keymap.set("n", "d<cr>", function()
+  vim.ui.open("https://numbat.dev/doc/")
+end, { buf = 0 })
