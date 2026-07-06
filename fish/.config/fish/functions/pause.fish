225058368e6c54e840ab140b78c4515b12bc3c23
David Marchante <davidmarchan@gmail.com>
2026-07-05 20:28:11 -0400

    chore(fish): remove pause functions

--- fish/.config/fish/functions/pause.fish
+++
@@ -1,4 +0,0 @@
-function pause
-    printf '\e[1m--- Press ENTER to continue ---\e[0m\\n'
-    exec head -n1
-end
--- fish/.config/fish/functions/pause_if_err.fish
+++
@@ -1,4 +0,0 @@
-function pause_if_err
-    # command; pause_if_err
-    test $status = 0 -o $status = 130; or pause
-end
