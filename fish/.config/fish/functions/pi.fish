0a1a6033fd448ba1ad87fb3769c9b3483b1283cd
David Marchante <davidmarchan@gmail.com>
2026-07-17 19:20:49 -0400

    feat(fish): allow `pi` to receive multiple packages

--- fish/.config/fish/functions/pi.fish
+++ fish/.config/fish/functions/pi.fish
@@ -1,11 +1,15 @@
 function pi --wraps="yay -Sii"
-    log_step $(green $argv) info
-    yay -Sii $argv
+    for package in $argv
+        log_step $(green $package) info
+        yay -Sii $package
 
-    log_step Installed executables
-    pacman -Qlq $argv | while read -l f
-        if test -f "$f"; and test -x "$f"
-            echo "$f"
+        if pacman -Qq -- $package >/dev/null 2>&1
+            log_step Installed executables
+            pacman -Qlq --color=always -- $package | while read -l f
+                if test -f "$f"; and test -x "$f"
+                    echo "$f"
+                end
+            end
         end
     end
 end
