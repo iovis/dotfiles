1c783d5269788d34f5b4fc5891671301126fde6d
David Marchante <davidmarchan@gmail.com>
2026-08-30 13:17:23 -0400

    feat(nvim.ft.gitcommit): use ctrl-c to abort commit

--- nvim/.config/nvim/after/ftplugin/gitcommit.lua
+++ nvim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -3,5 +3,6 @@ vim.opt_local.spelllang = "en_us"
 vim.opt_local.formatoptions = "toqrn1j"
 vim.opt_local.comments:append({ "b:-", "b:*" })
 
-vim.keymap.set("n", "q", "<cmd>wq<cr>", { buf = 0, nowait = true, silent = true })
+vim.keymap.set("n", "q", "<cmd>wq<cr>", { buf = 0, nowait = true })
+vim.keymap.set("n", "<c-c>", "<cmd>cquit<cr>", { buf = 0 })
 vim.keymap.set("i", "<c-h>", "<esc><cmd>Glg -500<cr><c-w><c-w>a", { buf = 0, silent = true })
