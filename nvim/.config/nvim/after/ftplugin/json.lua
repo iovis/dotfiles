d843a18fb2e40ffcaf7225a9caddfdaf70960e2d
David Marchante <davidmarchan@gmail.com>
2026-09-26 11:53:42 -0400

    feat(swaync): add notification sound for aerc
    Need to copy the default config or it will get overwritten (not
    composed). It lives in /etc/xdg/swaync/config.json

--- nvim/.config/nvim/after/ftplugin/json.lua
+++ nvim/.config/nvim/after/ftplugin/json.lua
@@ -16,6 +16,20 @@ if u.current_file() == "package.json" then
       end,
     })
   end
+elseif u.current_file():match("swaync/") then
+  -- Only clean up autocmds belonging to this buffer
+  local augroup = vim.api.nvim_create_augroup("swaync_reload", { clear = false })
+  vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+
+  -- /etc/xdg/swaync/config.json
+  vim.api.nvim_create_autocmd("BufWritePost", {
+    desc = "Reload SwayNC on save",
+    group = augroup,
+    buffer = vim.api.nvim_get_current_buf(),
+    callback = function()
+      vim.cmd("silent !systemctl --user restart swaync.service")
+    end,
+  })
 end
 
 vim.keymap.set("n", "'J", ":silent %!jq '.'<left>", { buf = 0 })
